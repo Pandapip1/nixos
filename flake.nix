f2338ab6b6bc89dde6047e7e44bcba4853d0e39d
Pandapip1 <>
2024-02-20 12:29:20 -0600

    Try this?

--- flake.nix
+++ flake.nix
@@ -10,32 +10,35 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }: 
-    let
-      pkgs = import nixpkgs { inherit system; };
-      baseConfiguration = ./configuration.nix;
+  outputs = { self, nixpkgs, flake-utils, ... }: {
+    packages = flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        baseConfiguration = ./configuration.nix;
 
-      makeConfig = { name, extraConfig }: 
-        let
-          config = import baseConfiguration { inherit pkgs; };
-        in
-        {
-          imports = [ baseConfiguration ];
-          networking.hostName = name;
-        } // extraConfig;
-    in
-    {
-      nixosConfigurations = with pkgs.nixos.lib; {
-        gavin-laptop-nixos-1 = nixosSystem {
-          inherit system;
-          modules = [
-            (makeConfig {
-              name = "gavin-laptop-nixos-1";
-              extraConfig = {
-              };
-            })
-          ];
+        makeConfig = { name, extraConfig }: 
+          let
+            config = import baseConfiguration { inherit pkgs; };
+          in
+          {
+            imports = [ baseConfiguration ];
+            networking.hostName = name;
+          } // extraConfig;
+      in
+      {
+        nixosConfigurations = with pkgs.nixos.lib; {
+          gavin-laptop-nixos-1 = nixosSystem {
+            inherit system;
+            modules = [
+              (makeConfig {
+                name = "gavin-laptop-nixos-1";
+                extraConfig = {
+                };
+              })
+            ]
+          };
         };
-      };
-    };
+      }
+    );
+  }
 }
