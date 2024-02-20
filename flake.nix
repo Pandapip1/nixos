4db77d53aac6f909b57024375046bb66abbbbb51
Pandapip1 <>
2024-02-20 12:21:35 -0600

    Fingers Crossed This Works

--- flake.nix
+++ flake.nix
@@ -12,24 +12,29 @@
 
   outputs = { self, nixpkgs, flake-utils, ... }:  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
+      pkgs = import nixpkgs { inherit system; };
       baseConfiguration = ./configuration.nix;
 
-      makeConfig = { name, extraConfig }: {
-        imports = [ baseConfiguration ];
-        networking.hostName = name;
-        ... extraConfig ...
-      };
+      makeConfig = { name, extraConfig }: 
+        let
+          config = import baseConfiguration { inherit pkgs; };
+        in
+        {
+          imports = [ baseConfiguration ];
+          networking.hostName = name;
+        } // extraConfig;
     in
     {
-      nixosConfigurations = {
-        gavin-laptop-nixos-1 = makeConfig {
-          name = "gavin-laptop-nixos-1";
-          extraConfig = {
-          };
+      nixosConfigurations = with pkgs.nixos.lib; {
+        gavin-laptop-nixos-1 = nixosSystem {
+          inherit system;
+          modules = [
+            (makeConfig {
+              name = "gavin-laptop-nixos-1";
+              extraConfig = {
+              };
+            })
+          ];
         };
       };
     }
