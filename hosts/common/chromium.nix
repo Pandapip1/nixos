20bcf27f000d33c519b565a57b92cfb8767631c0
Pandapip1 <>
2024-02-21 15:57:53 -0600

    Switch to stock chromium

--- hosts/common/chromium.nix
+++ hosts/common/chromium.nix
@@ -1,26 +1,9 @@
 { config, lib, pkgs, nix-software-center, nixos-conf-editor, ... }:
 
-let
-  extensionInstallForcelist = [
-    "ddkjiahejlhfcafbddmgiahcphecmpfh"
-    "oboonakemofpalcgghocfoadofidjkkk"
-    "nfcdcdoegfnidkeldipgmhbabmndlhbf"
-    "mpbjkejclgfgadiemmefgebjfooflfhl"
-    "icallnadddjmdinamnolclfjanhfoafe"
-    "pfldomphmndnmmhhlbekfbafifkkpnbc"
-    "nakplnnackehceedgkgkokbgbmfghain"
-    "mhfjchmiaocbleapojmgnmjfcmanihio"
-    "enamippconapkdmgfgjchkhakpfinmaj"
-    "omkfmpieigblcllmkgbflkikinpkodlk"
-    "gebbhagfogifgggkldgodflihgfeippi"
-    "mnjggcdmjocbbbhaepdhchncahnbgone"
-    "khncfooichmfjbepaaaebmommgaepoid"
-  ];
-in
 {
   # Configure installed packages
   environment.systemPackages = with pkgs; [
-    ungoogled-chromium
+    chromium
   ];
   environment.gnome.excludePackages = with pkgs; with pkgs.gnome; [
     epiphany
@@ -35,33 +18,22 @@ in
       "DefaultSearchProviderSearchURL" = "https://duckduckgo.com/?q={searchTerms}&ie={inputEncoding}";
       "DefaultSearchProviderSuggestURL" = "https://ac.duckduckgo.com/ac/?q={searchTerms}";
       "BlockExternalExtensions" = false;
-      "ExtensionInstallForcelist" = extensionInstallForcelist;
+      "ExtensionInstallForcelist" = [
+        "ddkjiahejlhfcafbddmgiahcphecmpfh"
+        "oboonakemofpalcgghocfoadofidjkkk"
+        "nfcdcdoegfnidkeldipgmhbabmndlhbf"
+        "mpbjkejclgfgadiemmefgebjfooflfhl"
+        "icallnadddjmdinamnolclfjanhfoafe"
+        "pfldomphmndnmmhhlbekfbafifkkpnbc"
+        "nakplnnackehceedgkgkokbgbmfghain"
+        "mhfjchmiaocbleapojmgnmjfcmanihio"
+        "enamippconapkdmgfgjchkhakpfinmaj"
+        "omkfmpieigblcllmkgbflkikinpkodlk"
+        "gebbhagfogifgggkldgodflihgfeippi"
+        "mnjggcdmjocbbbhaepdhchncahnbgone"
+        "khncfooichmfjbepaaaebmommgaepoid"
+      ];
     };
     mode = "0444";
   };
-  systemd.services.forceChromiumFlags = {
-    enable = true;
-    description = "Forces Ungoogled Chromium to use certain flags";
-    wantedBy = [ "multi-user.target" ];
-    path = with pkgs; [ jq ];
-    script = ''
-      for userdir in /home/*; do
-        user=$(basename "$userdir")
-        local_state="/home/$user/.config/chromium/Local State"
-        if [ -f /home/$user/.config/chromium/Local\ State ]; then
-          echo -E "$(jq '.browser.enabled_labs_experiments |= (["extension-mime-request-handling@2"] + . | unique)' "$local_state")" > "$local_state"
-        else
-          echo -E '{"browser": {"enabled_labs_experiments": ["extension-mime-request-handling@2"]}}' > "$local_state"
-        fi
-      done
-    '';
-    serviceConfig.Restart="on-failure";
-  };
-  programs.chromium.extensions = map (ext: {
-    id = ext;
-    version = "latest";
-    crxPath = builtins.fetchurl {
-      url = "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=121.0.6167.184&acceptformat=crx2,crx3&x=id%3D${ext}%26uc";
-    };
-  }) extensionInstallForcelist;
 }
