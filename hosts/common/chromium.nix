7fd59fe835125a963de6c2f8d4f20ba53826f0ee
Pandapip1 <>
2024-02-21 14:51:28 -0600

    hopefully the last one... probably not

--- hosts/common/chromium.nix
+++ hosts/common/chromium.nix
@@ -65,7 +65,7 @@ in
     script = builtins.concatStringsSep "\n" ([ "rm -rf /usr/share/chromium/extensions" "mkdir -p /usr/share/chromium/extensions" ] ++ (map (ext: ''
       mkdir -p /usr/share/chromium/extensions/${ext}
       curl -L -o "/usr/share/chromium/extensions/${ext}.crx" "https://clients2.google.com/service/update2/crx?response=redirect&prodversion=121.0.6167.184&acceptformat=crx2,crx3&x=id%3D${ext}%26uc"
-      unzip /usr/share/chromium/extensions/${ext}.crx -d /usr/share/chromium/extensions/${ext}
+      unzip /usr/share/chromium/extensions/${ext}.crx -d /usr/share/chromium/extensions/${ext} || true
       echo -E '{"external_crx": "/usr/share/chromium/extensions/${ext}.crx", "external_version": "'$(jq -r ".version" /usr/share/chromium/extensions/${ext}/manifest.json)'"}' > "/usr/share/chromium/extensions/${ext}.json"
       rm -rf /usr/share/chromium/extensions/${ext}
     '') extensionInstallForcelist));
