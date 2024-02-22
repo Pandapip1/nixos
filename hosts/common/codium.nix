5da4179b1f49aa1428582eb5b8d9a40919ced181
Pandapip1 <>
2024-02-22 08:57:22 -0600

    Split codium config

---
+++ hosts/common/codium.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, nix-software-center, nixos-conf-editor, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+        ms-python.python
+        ms-azuretools.vscode-docker
+        ms-vscode-remote.remote-ssh
+        vscode-extensions.james-yu.latex-workshop
+        github.vscode-pull-request-github
+        github.vscode-github-actions
+        github.copilot
+        github.copilot-chat
+        github.codespaces
+        gitlab.gitlab-workflow
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "remote-ssh-edit";
+          publisher = "ms-vscode-remote";
+          version = "0.47.2";
+          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+        }
+      ];
+    })
+  ];
+}
--- hosts/common/gnome-x11.nix
+++ hosts/common/gnome-x11.nix
@@ -40,29 +40,6 @@
     nix-software-center.packages.${system}.nix-software-center
     nixos-conf-editor.packages.${system}.nixos-conf-editor
     gnomeExtensions.remove-alttab-delay-v2
-    (vscode-with-extensions.override {
-      vscode = vscodium;
-      vscodeExtensions = with vscode-extensions; [
-        bbenoist.nix
-        ms-python.python
-        ms-azuretools.vscode-docker
-        ms-vscode-remote.remote-ssh
-        vscode-extensions.james-yu.latex-workshop
-        github.vscode-pull-request-github
-        github.vscode-github-actions
-        github.copilot
-        github.copilot-chat
-        github.codespaces
-        gitlab.gitlab-workflow
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          name = "remote-ssh-edit";
-          publisher = "ms-vscode-remote";
-          version = "0.47.2";
-          sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-        }
-      ];
-    })
   ];
   services.xserver.excludePackages = with pkgs; [
     xterm
