db4fac9bf765103d1b226090fd8b3f9e0b618cde
Gavin John <>
2024-02-29 17:02:00 -0600

    Temporarily add a bunch of libraries

--- hosts/gavin-laptop-nixos-1/module.nix
+++ hosts/gavin-laptop-nixos-1/module.nix
@@ -24,4 +24,28 @@
     # Users
     ../common/users/gavin.nix
   ];
+
+
+  environment.systemPackages = with pkgs; [
+    cmake
+    eigen
+    gcc
+    python3
+    vulkan-loader
+    vulkan-headers
+    glslang
+    libusb1
+    libudev-zero
+    libv4l
+    xorg.libxcb
+    xorg.xrandr
+    openhmd
+    doxygen
+    wayland-protocols
+    hidapi
+    opencv
+    libuvc
+    libjpeg
+    SDL2
+  ];
 }
