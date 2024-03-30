389eea97ef76aa9766546373484b1c81597ae6df
Gavin John <>
2024-03-30 11:00:01 -0500

    Fix config

--- hosts/common/services/nebula.nix
+++ hosts/common/services/nebula.nix
@@ -4,9 +4,9 @@
   environment.systemPackages = with pkgs; [
     nebula
   ];
-  services.nebula.networks.mesh = {
-    enable = false;
-    isLighthouse = true;
+  services.nebula.networks.nebula0 = {
+    enable = true;
+    isLighthouse = false;
     cert = builtins.readFile "../../../config/nebula/${hostname}.crt";
     key = "/etc/secrets/nebula/${hostname}.key";
     ca = builtins.readFile "../../../config/nebula/ca.crt";
