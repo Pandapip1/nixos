6ed25edaac482f40ef6d6c970634f52738bb1c23
Gavin John <>
2024-03-30 16:45:10 -0500

    Fine, I'll do it myself

--- hosts/common/services/nebula.nix
+++ hosts/common/services/nebula.nix
@@ -1,57 +1,31 @@
-{ self, hostname, config, lib, pkgs, ... }:
+{ self, hostname, config, lib, pkgs, systemd, ... }:
 
-{
+rec {
   environment.systemPackages = with pkgs; [
     nebula
   ];
-  services.nebula.networks.nebula0 = {
+  systemd.services."nebula@nebula0-fixed" = {
     enable = true;
-    isLighthouse = false;
-    cert = builtins.toFile "nebula-${hostname}.crt" (builtins.readFile "${self}/config/nebula/${hostname}.crt");
-    key = "/etc/secrets/nebula/${hostname}.key";
-    ca = builtins.toFile "nebula-ca.crt" (builtins.readFile "${self}/config/nebula/ca.crt");
-    lighthouses = [
-      "10.0.0.1"
-    ];
-    staticHostMap = {
-      "10.0.0.1" = [
-        "64.23.155.119:4242"
-        # TODO: IPv6
-        "lighthouse.3webs.org:4242"
-      ];
+    description = "Fixed nebula VPN service for nebula0";
+    wants = [ "basic.target" "network-online.target" "nss-lookup.target" "time-sync.target" ];
+    after = [ "basic.target" "network.target" "network-online.target" ];
+    before = [ "sshd.service" ];
+    serviceConfig = let ExecStart = systemd.services."nebula@nebula0".serviceConfig.ExecStart; in {
+      Type = "notify";
+      NotifyAccess = "main";
+      SyslogIdentifier = "nebula0";
+      ExecReload = "/run/current-system/sw/bin/kill -HUP $MAINPID";
+      ExecStart = "/run/current-system/sw/bin/nebula -config ${
+        builtins.toFile "nebula-config-${hostname}.yml" (
+          "pki:\n" +
+          "  ca: ${builtins.toFile "nebula-ca.crt" (builtins.readFile "${self}/config/nebula/ca.crt")}\n" +
+          "  cert: ${builtins.toFile "nebula-${hostname}.crt" (builtins.readFile "${self}/config/nebula/${hostname}.crt")}\n" +
+          "  key: /etc/secrets/nebula/${hostname}.key\n\n" +
+          builtins.readFile "${self}/config/nebula/config-${hostname}.yml"
+        )
+      }";
+      Restart = "always";
     };
-    tun = {
-      disable = false;
-      device = "nebula0";
-    };
-    firewall = {
-      inbound = [ 
-        # No rules yet
-      ];
-      outbound = [
-        { # Allow all outbound traffic
-          host = "any";
-          port = "any";
-          proto = "any";
-        }
-      ];
-    };
-  };
-  systemd.services."nebula@nebula0".serviceConfig = {
-    User = lib.mkForce "root";
-    Group = lib.mkForce "root";
-    LockPersonality = lib.mkForce false;
-    NoNewPrivileges = lib.mkForce false;
-    ProtectClock = lib.mkForce false;
-    ProtectControlGroups = lib.mkForce false;
-    ProtectHome = lib.mkForce false;
-    ProtectHostname = lib.mkForce false;
-    ProtectKernelLogs = lib.mkForce false;
-    ProtectKernelModules = lib.mkForce false;
-    ProtectKernelTunables = lib.mkForce false;
-    ProtectProc = lib.mkForce "default";
-    ProtectSystem = lib.mkForce false;
-    RestrictNamespaces = lib.mkForce false;
-    RestrictSUIDSGID = lib.mkForce false;
+    unitConfig.StartLimitIntervalSec = 0;
   };
 }
