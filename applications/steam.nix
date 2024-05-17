201798726decb6a81121d97bc1ea6b41d6512b89
Pandapip1 <>
2024-05-16 22:51:11 -0500

    Better steam stuff

--- applications/steam.nix
+++ applications/steam.nix
@@ -1,22 +1,32 @@
 { config, lib, pkgs, ... }:
-
+let
+  steam = pkgs.steam.override {
+  };
+in
 {
   environment.systemPackages = with pkgs; [
-    steam
-    steam-run
     gamescope
     gamemode
     mangohud
     vkbasalt
     vkbasalt-cli
-  ];
+  ] ++ [ steam steam.run ];
   programs.steam = {
     enable = true;
+    package = steam;
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for local network game transfers
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     gamescopeSession.enable = false; # Manually invoke gamescope
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
   programs.gamescope = {
     enable = true;
+    capSysNice = true;
+    args = [
+      "-f"
+    ];
   };
 }
