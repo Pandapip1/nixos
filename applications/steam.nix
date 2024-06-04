265ee104082ae3911e2cc374c37cfdcee635ed87
Gavin John <>
2024-06-03 19:53:49 -0500

    Gaming computer V2

--- applications/steam.nix
+++ applications/steam.nix
@@ -1,10 +1,22 @@
 { pkgs, ... }:
 let
   steam = pkgs.steam.override {
+    extraPkgs = pkgs: with pkgs; [
+      gamescope
+    ];
+  };
+  lutris = pkgs.lutris.override {
+    extraLibraries =  pkgs: [
+      # List library dependencies here
+    ];
+    extraPkgs = pkgs: [
+      # List package dependencies here
+    ];
   };
 in
 {
   environment.systemPackages = with pkgs; [
+    lutris
     gamescope
     gamemode
     mangohud
@@ -17,7 +29,9 @@ in
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for local network game transfers
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    gamescopeSession.enable = false; # Manually invoke gamescope
+    gamescopeSession.enable = true; # Automatically invoke gamescope
+    protontricks.enable = true;
+    extest.enable = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
     ];
--- hosts/gavin-gaming-desktop.nix
+++ hosts/gavin-gaming-desktop.nix
@@ -15,31 +15,33 @@
     # Users
     ../users/gavin.nix
   ];
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
+  
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/e287de45-0d59-4eae-a1c0-fe0f882320c5";
+    { device = "/dev/disk/by-uuid/9cc16f16-51cf-4556-bcdb-647f1ef0ced1";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/D10B-109F";
+    { device = "/dev/disk/by-uuid/6181-AF10";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/4540c1ff-03b9-4119-a013-1fe791c5a6de"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp4s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
