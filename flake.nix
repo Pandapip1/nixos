d982ff1114d3affd31b9789a9d18a116bc626fb8
Gavin John <gavinnjohn@gmail.com>
2024-07-05 11:42:37 -0400

    A couple tweaks & update patches

--- flake.nix
+++ flake.nix
@@ -23,8 +23,8 @@
       nixosConfigurations = builtins.listToAttrs (map (hostname: let
         system = "x86_64-linux";
         pkgs-unpatched = import nixpkgs { inherit system; };
-        nixpkgs-patched-src = pkgs-unpatched.stdenvNoCC.mkDerivation {
-          name = "nixpkgs-patched-src";
+        nixpkgs-patched-source = pkgs-unpatched.stdenvNoCC.mkDerivation {
+          name = "nixpkgs-patched-source";
           src = nixpkgs;
 
           patches = let
@@ -36,24 +36,19 @@
               hash = "sha256-0kZDccC4WaKL+y3QEypQjeRx3AO1mE2wZqPyN8b6bVE=";
             })
             (fetchpatch {
-              name = "init-envision.patch";
-              url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/321015.patch";
-              hash = "sha256-iR8QTux5nN6TgeU/ishrNXG64beVqwRDoy+3AnMwB3U=";
-            })
-            (fetchpatch {
               name = "init-stardust-xr-server.patch";
               url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/324375.patch";
-              hash = "sha256-BOLn7YhYo3qd4Wlm21RVqvwHabHMbFP6QH82fP+1lao=";
+              hash = "sha256-6Se3ZKvb7N/Z1Mo4Bx9B868PlTqVXJo4koL0hXE0u/E=";
             })
             (fetchpatch {
               name = "init-stardust-xr-flatland.patch";
               url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/324395.patch";
-              hash = "sha256-xkF815z5r38jTuUtsohq+IKt5cdu6xPxDEsMx3Rz1Hs=";
+              hash = "sha256-VtIC40bHzlx9iYktttd7nsi3WKs3h00u3OalSTguNbc=";
             })
             (fetchpatch {
               name = "init-stardust-xr-kiara.patch";
               url = "https://patch-diff.githubusercontent.com/raw/NixOS/nixpkgs/pull/324404.patch";
-              hash = "sha256-z4aBQYtuE9VeNZy4+BtoZW1boFBCH+d3AXKlre9bCOE=";
+              hash = "sha256-qAIt5YbJPxewM3wnbTxo24LgmewtQ8MDpKDuzdYJdls=";
             })
           ];
           nativeBuildInputs = [ pkgs-unpatched.nix ];
@@ -69,7 +64,7 @@
           '';
           dontFixup = true;
         };
-        nixpkgs-patched = getFlake "${nixpkgs-patched-src}";
+        nixpkgs-patched = getFlake "${nixpkgs-patched-source}";
         pkgs = (import nixpkgs-patched { inherit system; config.allowUnfree = true; });
       in {
         name = hostname;
