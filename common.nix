4f38bfbe05ebef815c287f4566f6c13bde38322b
Gavin John <gavinnjohn@gmail.com>
2024-08-28 17:56:46 -0700

    Custom nix GC

--- common.nix
+++ common.nix
@@ -13,6 +13,24 @@
     };
   };
 
+  # Auto GC every day
+  systemd.services.nix-gc = let
+    configurationLimit = 16;
+  in {
+    description = "Nix Garbage Collector";
+    script = ''
+      ${lib.getExe' pkgs.nix "nix-env"} --delete-generations +${toString configurationLimit} --profile /nix/var/nix/profiles/system
+      exec ${lib.getExe' pkgs.nix "nix-collect-garbage"}
+    '';
+    serviceConfig.Type = "oneshot";
+    startAt = "daily";
+  };
+  systemd.timers.nix-gc = lib.mkIf cfg.automatic {
+    timerConfig = {
+      Persistent = true;
+    };
+  };
+
   hardware.enableAllFirmware = true;
 
   time.timeZone = lib.mkForce null;
