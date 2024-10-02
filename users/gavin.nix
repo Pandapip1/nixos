55f57db0ba70f1ac29c7614661185516a7b276ed
Gavin John <gavinnjohn@gmail.com>
2024-10-01 22:55:20 -0700

    Add dialout group to myself

--- users/gavin.nix
+++ users/gavin.nix
@@ -4,7 +4,7 @@
   users.users.gavin = {
     isNormalUser = true;
     description = "gavin";
-    extraGroups = [ "networkmanager" "wheel" "disk" "corectrl" ];
+    extraGroups = [ "networkmanager" "wheel" "disk" "corectrl" "dialout" ];
     packages = with pkgs; [ ];
     openssh = {
       authorizedKeys.keys = [
