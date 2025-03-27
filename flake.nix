13a501ef8b2451664b43cc68d9f69cb2645f8c52
Gavin John <gavinnjohn@gmail.com>
2025-03-27 11:32:07 -0700

    Update nixpkgs to unpatched

--- flake.nix
+++ flake.nix
@@ -64,20 +64,21 @@
               value =
                 let
                   pkgs-unpatched = import nixpkgs { inherit system; };
-                  nixpkgs-patched-source = pkgs-unpatched.applyPatches {
-                    name = "nixpkgs-patched-source";
-                    src = nixpkgs;
-                    patches =
-                      let
-                        fetchpatch = pkgs-unpatched.fetchpatch;
-                      in
-                      [
-                      ];
-                  };
-                  nixpkgs-patched = getFlake "${nixpkgs-patched-source}";
+                  #nixpkgs-patched-source = pkgs-unpatched.applyPatches {
+                  #  name = "nixpkgs-patched-source";
+                  #  src = nixpkgs;
+                  #  patches =
+                  #    let
+                  #      fetchpatch = pkgs-unpatched.fetchpatch;
+                  #    in
+                  #    [
+                  #    ];
+                  #};
+                  #nixpkgs-patched = getFlake "${nixpkgs-patched-source}";
                   modules = map (s: "${modulesDir}/${s}") (builtins.attrNames (builtins.readDir modulesDir));
                 in
-                nixpkgs-patched.lib.nixosSystem {
+                #nixpkgs-patched.lib.nixosSystem {
+                nixpkgs.lib.nixosSystem
                   inherit system;
                   specialArgs = inputs // {
                     inherit system hostname;
