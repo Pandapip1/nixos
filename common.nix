6e7d9dd8739fe0f409f1feccca191a292a00b324
Gavin John <gavinnjohn@gmail.com>
2025-03-27 11:17:58 -0700

    Add documentation options to common.nix

--- common.nix
+++ common.nix
@@ -34,6 +34,23 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  documentation = {
+    enable = lib.mkDefault true;
+    man = {
+      enable = lib.mkDefault true;
+      man-db.enable = lib.mkDefault false;
+      mandoc.enable = lib.mkDefault true; # BSD-compatible
+      generateCaches = lib.mkDefault true;
+    };
+    doc.enable = lib.mkDefault true;
+    dev.enable = lib.mkDefault true;
+    info.enable = lib.mkDefault true;
+    nixos = {
+      enable = lib.mkDefault true;
+      includeAllModules = lib.mkDefault true;
+    };
+  };
+
   boot = {
     loader = {
       systemd-boot = {
