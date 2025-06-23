4514d19c638e1ec5a561ff910cd358f07a73cd16
Gavin John <gavinnjohn@gmail.com>
2025-06-23 15:43:12 -0400

    Fix string issue

--- hosts/x86_64-linux/berry.pandapip1.com.nix
+++ hosts/x86_64-linux/berry.pandapip1.com.nix
@@ -97,7 +97,7 @@
       enableACME = true;
       forceSSL = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${config.services.node-red.port}";
+        proxyPass = "http://127.0.0.1:${toString config.services.node-red.port}";
         proxyWebsockets = true;
       };
     };
