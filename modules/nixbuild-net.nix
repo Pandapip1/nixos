9361ffc9c3d80c9b4a9a357d38f71fa893be22e6
Gavin John <gavinnjohn@gmail.com>
2025-07-01 14:22:38 -0400

    And fix nixbuild

--- modules/nixbuild-net.nix
+++ modules/nixbuild-net.nix
@@ -1,10 +1,14 @@
-
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg = config.programs.nixbuild-net;
-
-in {
+in
+{
   options = {
     nixbuild-net = {
       enable = lib.mkEnableOption "nixbuild.net";
@@ -12,32 +16,31 @@ in {
   };
 
   config = mkIf cfg.enable {
-    {
-      programs.ssh.extraConfig = ''
-        Host eu.nixbuild.net
-        PubkeyAcceptedKeyTypes ssh-ed25519
-        ServerAliveInterval 60
-        IPQoS throughput
-        IdentityFile /etc/secrets/id_nixbuild-net
-      '';
+    programs.ssh.extraConfig = ''
+      Host eu.nixbuild.net
+      PubkeyAcceptedKeyTypes ssh-ed25519
+      ServerAliveInterval 60
+      IPQoS throughput
+      IdentityFile /etc/secrets/nixbuild/id_nixbuild-net
+    '';
 
-      programs.ssh.knownHosts = {
-        nixbuild = {
-          hostNames = [ "eu.nixbuild.net" ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
-        };
+    programs.ssh.knownHosts = {
+      nixbuild = {
+        hostNames = [ "eu.nixbuild.net" ];
+        publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPIQCZc54poJ8vqawd8TraNryQeJnvH1eLpIDgbiqymM";
       };
+    };
 
-      nix = {
-        distributedBuilds = true;
-        buildMachines = [
-          { hostName = "eu.nixbuild.net";
-            system = "x86_64-linux";
-            maxJobs = 100;
-            supportedFeatures = [ "benchmark" "big-parallel" ];
-          }
+    nix = {
+      distributedBuilds = true;
+      settings = {
+        trusted-substituters = [
+          "ssh://eu.nixbuild.net"
         ];
-      };
-    }
+        trusted-public-keys = [
+          "nixbuild.net/WD7ZMS-1:fDoljU8vNe12BOadKcF0jxTvRddZjIjUkoyZCm7QMFw="
+        ];
+      }
+    };
   };
 }
