2e562d8588cf7379582ceab4c607f27e70b6787c
Gavin John <gavinnjohn@gmail.com>
2025-07-01 13:46:24 -0400

    Update stevenblack blocklist

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-compat.url = "github:nix-community/flake-compat";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     hosts = {
-      url = "github:Pandapip1/hosts/patch-1";
+      url = "github:Stevenblack/hosts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-index-database = {
