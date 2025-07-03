06b8992cc4f60bbd6b136eb626b36ce96003f727
Gavin John <gavinnjohn@gmail.com>
2025-07-03 16:45:43 -0400

    Disable open-webui auth

--- hosts/x86_64-linux/gavin-laptop-nixos-1.nix
+++ hosts/x86_64-linux/gavin-laptop-nixos-1.nix
@@ -70,6 +70,7 @@
     openFirewall = false; # Local-only, thank you
     host = "localhost"; # Again, still local-only please
     port = 7890; # Just a random free port
+    environment.WEBUI_AUTH = "False";
   };
   services.ollama = {
     enable = true;
