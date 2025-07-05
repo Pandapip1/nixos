6bd56fdef98aa401ae1a36e61dc2fa5dd0a7e7b4
Gavin John <gavinnjohn@gmail.com>
2025-07-05 18:02:30 -0400

    Undo hardening disable

--- hosts/aarch64-linux/berry.pandapip1.com.nix
+++ hosts/aarch64-linux/berry.pandapip1.com.nix
@@ -127,11 +127,6 @@
   secrets.inadyn.ownership = {
     inherit (config.services.inadyn) user group;
   };
-  # Clearly has a bug
-  systemd.services.inadyn.serviceConfig = {
-    LoadCredential = lib.mkForce [];
-    ProtectSystem = lib.mkForce false;
-  };
 
   # Allow nginx user to see ACME certs
   # "Certificate berry.pandapip1.com (group=acme) must be readable by service(s) nginx.service (user=nginx groups=nginx), nginx-config-reload.service (user=root groups=)"
