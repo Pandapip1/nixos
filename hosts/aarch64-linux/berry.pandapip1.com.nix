1bc11b88e32600a925db15e5a81fa872643a4be1
Gavin John <gavinnjohn@gmail.com>
2025-07-05 17:52:24 -0400

    Fix potential sandboxing bug with inadyn

--- hosts/aarch64-linux/berry.pandapip1.com.nix
+++ hosts/aarch64-linux/berry.pandapip1.com.nix
@@ -125,6 +125,7 @@
     configFile = "/etc/secrets/inadyn/inadyn.conf";
   };
   secrets.inadyn.ownership.user = config.services.inadyn.user;
+  systemd.services.inadyn.serviceConfig.LoadCredential = lib.mkForce []; # Clearly has a bug
 
   # Allow nginx user to see ACME certs
   # "Certificate berry.pandapip1.com (group=acme) must be readable by service(s) nginx.service (user=nginx groups=nginx), nginx-config-reload.service (user=root groups=)"
