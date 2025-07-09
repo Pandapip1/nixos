1a27b34de9ef1c50dfd8981b21f36770854a55c5
Gavin John <gavinnjohn@gmail.com>
2025-07-09 12:49:32 -0400

    Oops

--- services/nebula.nix
+++ services/nebula.nix
@@ -14,41 +14,39 @@ in
   ];
   services.nebula.networks.nebula0 = {
     enable = true;
+    ca = "${self}/config/nebula/ca.crt";
+    cert = "${self}/config/nebula/${hostname}.crt";
+    key = "/etc/secrets/nebula/${hostname}.key";
+    staticHostMap = {
+      "10.0.0.1" = [
+        "64.23.155.119:4242"
+        "lighthouse.3webs.org:4242"
+      ];
+    };
+    lighthouses = [
+      "10.0.0.1"
+    ];
+    relays = [
+      "10.0.0.1"
+    ];
+    firewall = {
+      outbound = [
+        {
+          host = "any";
+          port = "any";
+          proto = "any";
+        }
+      ];
+      inbound = [
+        {
+          host = "any";
+          port = "any";
+          proto = "any";
+        }
+      ];
+    };
     settings = {
-      ca = "${self}/config/nebula/ca.crt";
-      cert = "${self}/config/nebula/${hostname}.crt";
-      key = "/etc/secrets/nebula/${hostname}.key";
-      staticHostMap = {
-        "10.0.0.1" = [
-          "64.23.155.119:4242"
-          "lighthouse.3webs.org:4242"
-        ];
-      };
-      lighthouses = [
-        "10.0.0.1"
-      ];
-      relays = [
-        "10.0.0.1"
-      ];
-      firewall = {
-        outbound = [
-          {
-            host = "any";
-            port = "any";
-            proto = "any";
-          }
-        ];
-        inbound = [
-          {
-            host = "any";
-            port = "any";
-            proto = "any";
-          }
-        ];
-      };
-      settings = {
-        punchy.punch = true;
-      };
+      punchy.punch = true;
     };
   };
 }
