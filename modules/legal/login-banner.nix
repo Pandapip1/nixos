fbedd6717a5c5902f838ded69357ba729911bc4c
Gavin John <gavinnjohn@gmail.com>
2025-07-18 14:03:53 -0400

    Move licensing to new 'legal' folder, and add /etc/issue banner

---
+++ modules/legal/login-banner.nix
@@ -0,0 +1,28 @@
+{
+  environment.etc.issue.text = ''
+    ********************************************************************************
+    *                                                                              *
+    *                             WARNING: AUTHORIZED USE ONLY                     *
+    *                                                                              *
+    ********************************************************************************
+
+    You are accessing a private computer system. This system, including all related
+    equipment, networks, and network devices (specifically including Internet access),
+    is provided only for authorized use. This system may be monitored for all lawful
+    purposes, including to ensure that its use is authorized, for management of the
+    system, to facilitate protection against unauthorized access, and to verify security
+    procedures, survivability, and operational security. Monitoring includes active
+    attacks by authorized entities to test or verify the security of this system.
+    During monitoring, information may be examined, recorded, copied, and used for
+    authorized purposes. All information, including personal information, placed on or
+    sent over this system may be monitored.
+
+    Use of this computer system, authorized or unauthorized, constitutes consent to
+    monitoring of this system. Unauthorized use may subject you to criminal prosecution.
+    Evidence of unauthorized use collected during monitoring may be used for administrative,
+    criminal, or other adverse action. Use of this system constitutes consent to monitoring
+    for these purposes.
+
+    ********************************************************************************
+  '';
+}
