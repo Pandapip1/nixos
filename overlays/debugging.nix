ed214a680be53d4ab0fda187781727b7d77edf74
Gavin John <gavinnjohn@gmail.com>
2025-07-19 13:31:08 -0400

    Fix debugging.nix asan enable

--- overlays/debugging.nix
+++ overlays/debugging.nix
@@ -3,7 +3,7 @@ let
   inherit (prev) lib;
 in
 {
-  geoclue2 = prev.enableDebugging prev.geoclue2.overrideAttrs (
+  geoclue2 = prev.enableDebugging (prev.geoclue2.overrideAttrs (
     _: previousAttrs: {
       mesonFlags = [
         (lib.mesonOption "b_sanitize" "address")
@@ -11,6 +11,6 @@ in
         (lib.mesonBool "b_pie" true)
       ] ++ previousAttrs.mesonFlags;
     }
-  );
+  ));
   wireplumber = prev.enableDebugging prev.wireplumber;
 }
