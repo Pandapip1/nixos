97d8b732cb7349e3600aa5fb8ecfba24b904667a
Gavin John <gavinnjohn@gmail.com>
2025-07-22 16:48:07 -0400

    Do some more hardening

--- modules/security/sysctl.nix
+++ modules/security/sysctl.nix
@@ -32,6 +32,21 @@ in
       protected_regular = 2;
     };
 
+    # Disable core dumps for SUID programs (I don't know exactly why this is good but I can't see it being actively harmful)
+    fs.suid_dumpable = 0;
+
+    # Hide kernel pointers to make attacks harder
+    kernel.kptr_restrict = 2;
+
+    # Disable SysRq key
+    kernel.sysrq = 0;
+
+    # Disable unprivileged bpf
+    kernel.unprivileged_bpf_disabled = 1;
+
+    # Enable hardening for the JIT compiler
+    net.core.bpf_jit_harden = 2;
+
     # Disable ICMP redirects
     net = {
       ipv4.conf = {
@@ -47,4 +62,11 @@ in
       };
     };
   };
+
+  # Log invalid packets
+  net.ipv4.conf.all.log_martians = 1;
+  net.ipv4.conf.default.log_martians = 1;
+
+  # Enable reverse path filtering
+  net.ipv4.conf.all.rp_filter = 1;
 }
