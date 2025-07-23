1e982ed6f3b4c85bba5f7ee4a50418f44d260090
Gavin John <gavinnjohn@gmail.com>
2025-07-23 10:13:04 -0400

    Unset before reassigning

--- modules/security/noroothistory.nix
+++ modules/security/noroothistory.nix
@@ -2,6 +2,8 @@
   environment.shellInit = ''
     # Disable storing root's executed commands
     if [ "$(id -u)" -eq 0 ]; then
+      unset HISTSIZE
+      unset HISTFILESIZE
       HISTSIZE=0
       HISTFILESIZE=0
       export HISTSIZE
