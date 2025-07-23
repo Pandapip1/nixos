fdb89c9ec256086ab71466c8fbbb700df785c72c
Gavin John <gavinnjohn@gmail.com>
2025-07-23 10:00:07 -0400

    Fix no-root-history hardening profile

--- modules/security/noroothistory.nix
+++ modules/security/noroothistory.nix
@@ -1,16 +1,12 @@
 {
-  pkgs,
-  ...
-}:
-
-{
-  environment.profiles = [
-    (pkgs.writeScriptBin "no-root-history" ''
+  environment.etc."profile.d".no-root-history = {
+    text = ''
       if [ "$(id -u)" -eq 0 ]; then
         unset HISTFILE
         export HISTSIZE=0
         export HISTFILESIZE=0
       fi
-    '')
-  ];
+    '';
+    mode = "0555";
+  };
 }
