0af9203f749cee2bd3595f2328db88515508442d
Gavin John <gavinnjohn@gmail.com>
2025-08-06 16:43:56 -0400

    Add polkit agent and pinentry package

--- modules/defaults/optin/workstation.nix
+++ modules/defaults/optin/workstation.nix
@@ -98,5 +98,22 @@
     # Use COSMIC greeter with KDE plasma :)
     services.displayManager.cosmic-greeter.enable = true;
     services.desktopManager.plasma6.enable = true;
+
+    # Add GNOME polkit agent
+    systemd.user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+    # And pinentry
+    programs.gnupg.agent.pinentryPackage = pkgs.pinentry-gnome3;
   };
 }
