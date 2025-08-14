0f71372ffcc63c05b970c097964819bb6c97cae9
Gavin John <gavinnjohn@gmail.com>
2025-08-14 15:56:17 -0400

    Add some additional vscode extensions

--- modules/defaults/vscode.nix
+++ modules/defaults/vscode.nix
@@ -21,11 +21,27 @@
       ms-python.python
       james-yu.latex-workshop
       bbenoist.nix
-      rust-lang.rust-analyzer
+      # rust-lang.rust-analyzer
       reditorsupport.r
       chrischinchilla.vscode-pandoc
-      # Language Packs
-      vscjava.vscode-java-pack
+      llvm-vs-code-extensions.vscode-clangd
+      ms-vscode.cmake-tools
+      myriad-dreamin.tinymist
+      (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "vscode-ros";
+          publisher = "ms-iot";
+          version = "0.9.6";
+          hash = "sha256-ZsGBzfLzRFzSWx4vttfXPb6gSrFBy+QUDz9hkAdKMCw=";
+        };
+      })
+      # (pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+      #   mktplcRef = {
+      #     name = "pddltools";
+      #     publisher = "kristianskovjohansen";
+      #     version = "1.6.0";
+      #   };
+      # })
       # "AI"
       # continue.continue
     ];
