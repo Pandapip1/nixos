1a55025d7c356b780a3da827ec58b5fe97f06f82
Gavin John <gavinnjohn@gmail.com>
2025-10-11 19:56:15 -0700

    ISTG

---
+++ modules/patches/paddleavx.nix
@@ -0,0 +1,12 @@
+{
+  nixpkgs.overlays = [
+    (_: prev: {
+      python312Packages.paddlepaddle = prev.python312Packages.paddlepaddle.overrideAttrs {
+        env.WITH_AVX = "OFF";
+      };
+      python313Packages.paddlepaddle = prev.python313Packages.paddlepaddle.overrideAttrs {
+        env.WITH_AVX = "OFF";
+      };
+    })
+  ]
+}
