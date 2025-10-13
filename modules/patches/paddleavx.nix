311144d610536c88ca44017c11f39272ee96c344
Gavin John <gavinnjohn@gmail.com>
2025-10-13 10:01:52 -0700

    Try overriding safetensors to remove paddle support

--- modules/patches/paddleavx.nix
+++ modules/patches/paddleavx.nix
@@ -2,13 +2,71 @@
   nixpkgs.overlays = [
     (_: prev: {
       python312Packages = prev.python312Packages // {
-        paddlepaddle = prev.python312Packages.paddlepaddle.overrideAttrs {
-          env.WITH_AVX = "OFF";
+        safetensors = prev.python312Packages.safetensors.overridePythonAttrs {
+          optional-dependencies = lib.fix (self: {
+            numpy = [ numpy ];
+            torch = self.numpy ++ [
+              torch
+            ];
+            tensorflow = self.numpy ++ [
+              tensorflow
+            ];
+            pinned-tf = self.numpy ++ [
+              tensorflow
+            ];
+            jax = self.numpy ++ [
+              flax
+              jax
+            ];
+            mlx = [
+              mlx
+            ];
+            testing = self.numpy ++ [
+              h5py
+              huggingface-hub
+              setuptools-rust
+              pytest
+              pytest-benchmark
+              hypothesis
+              fsspec
+            ];
+            all = self.torch ++ self.numpy ++ self.pinned-tf ++ self.jax ++ self.paddlepaddle ++ self.testing;
+            dev = self.all;
+          });
         };
       };
       python313Packages = prev.python313Packages // {
-        paddlepaddle = prev.python313Packages.paddlepaddle.overrideAttrs {
-          env.WITH_AVX = "OFF";
+        safetensors = prev.python313Packages.safetensors.overridePythonAttrs {
+          optional-dependencies = lib.fix (self: {
+            numpy = [ numpy ];
+            torch = self.numpy ++ [
+              torch
+            ];
+            tensorflow = self.numpy ++ [
+              tensorflow
+            ];
+            pinned-tf = self.numpy ++ [
+              tensorflow
+            ];
+            jax = self.numpy ++ [
+              flax
+              jax
+            ];
+            mlx = [
+              mlx
+            ];
+            testing = self.numpy ++ [
+              h5py
+              huggingface-hub
+              setuptools-rust
+              pytest
+              pytest-benchmark
+              hypothesis
+              fsspec
+            ];
+            all = self.torch ++ self.numpy ++ self.pinned-tf ++ self.jax ++ self.paddlepaddle ++ self.testing;
+            dev = self.all;
+          });
         };
       };
     })
