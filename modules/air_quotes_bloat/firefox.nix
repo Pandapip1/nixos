f7d93f4c7f83836ad7e141db2bcfe5794241cb0a
Gavin John <gavinnjohn@gmail.com>
2025-10-14 17:44:20 -0700

    Add some default firefox prefs

--- modules/air_quotes_bloat/firefox.nix
+++ modules/air_quotes_bloat/firefox.nix
@@ -7,5 +7,21 @@
 {
   programs.firefox = {
     enable = lib.mkDefault config.services.graphical-desktop.enable;
+    preferences = {
+      # Enable theming
+      "widget.gtk.libadwaita-colors.enabled" = false;
+
+      # Switch default search engine to DDG
+      "browser.search.defaultenginename" = "DuckDuckGo";
+      "browser.search.defaulturl" = "https://duckduckgo.com/?q=";
+
+      # Disable FF Password manager (I use KeePassXC)
+      "signon.rememberSignons" = false;
+      "signon.prefillForms" = false;
+      "browser.formfill.enable" = false;
+
+      # We're on NixOS, firefox can't update itself
+      "update_notifications.enabled" = false;
+    };
   };
 }
