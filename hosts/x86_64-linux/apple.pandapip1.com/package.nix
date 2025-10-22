15f71b22b157f6465b6e010abd548a5d315eca23
Gavin John <gavinnjohn@gmail.com>
2025-10-21 22:19:44 -0700

    Disable gnome keyring

--- hosts/x86_64-linux/apple.pandapip1.com/package.nix
+++ hosts/x86_64-linux/apple.pandapip1.com/package.nix
@@ -21,6 +21,7 @@
   # Use COSMIC DE
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
+  services.gnome.gnome-keyring.enable = false; # For some reason enabling cosmic enables gnome keyring. I want to use keepassxc thank you very much.
 
   # Enable nixbuild.net, but only for building to other systems
   nixbuild-net = {
