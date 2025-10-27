bc60d3110a27a22fe543c7c5f7c32d5bf5474112
Gavin John <gavinnjohn@gmail.com>
2025-10-26 21:59:37 -0700

    Switch back away from uwsm

--- hosts/x86_64-linux/edamame.pandapip1.com/additional-sessions.nix
+++ hosts/x86_64-linux/edamame.pandapip1.com/additional-sessions.nix
@@ -1,23 +1,37 @@
 {
-  lib,
   pkgs,
   ...
 }:
 
 {
-  programs.uwsm = {
-    enable = true;
-    waylandCompositors = {
-      stardustxr = {
-        prettyName = "Stardust XR";
-        comment = "TODO stardust-xr";
-        binPath = lib.getExe pkgs.stardust-xr-server;
-      };
-      kmscon = {
-        prettyName = "kmscon";
-        comment = "TODO kmscon";
-        binPath = lib.getExe pkgs.kmscon;
-      };
-    };
-  };
+  services.displayManager.sessionPackages = [
+    (
+      (pkgs.makeDesktopItem {
+        destination = "/share/wayland-sessions";
+        name = "stardust-xr";
+        comment = "TODO";
+        exec = lib.getExe pkgs.stardust-xr-server;
+        type = "Application";
+        desktopName = "Stardust XR";
+      }).overrideAttrs (oldAttrs: {
+        passthru = (oldAttrs.passthru or {}) // {
+          providedSessions = [ "stardust-xr" ];
+        };
+      })
+    )
+    (
+      (pkgs.makeDesktopItem {
+        destination = "/share/wayland-sessions";
+        name = "kmscon";
+        comment = "TODO";
+        exec = lib.getExe pkgs.kmscon;
+        type = "Application";
+        desktopName = "kmscon";
+      }).overrideAttrs (oldAttrs: {
+        passthru = (oldAttrs.passthru or {}) // {
+          providedSessions = [ "kmscon" ];
+        };
+      })
+    )
+  ];
 }
