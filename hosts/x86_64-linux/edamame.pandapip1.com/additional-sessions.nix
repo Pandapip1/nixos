db9957f53eea9deae7004f9b1eb719a5211b5ec6
Gavin John <gavinnjohn@gmail.com>
2025-10-26 21:46:39 -0700

    Switch to uwsm

--- hosts/x86_64-linux/edamame.pandapip1.com/additional-sessions.nix
+++ hosts/x86_64-linux/edamame.pandapip1.com/additional-sessions.nix
@@ -4,34 +4,19 @@
 }:
 
 {
-  services.displayManager.sessionPackages = [
-    (
-      (pkgs.makeDesktopItem {
-        destination = "/share/wayland-sessions";
-        name = "stardust-xr";
-        comment = "TODO";
-        exec = lib.getExe pkgs.stardust-xr-server;
-        type = "Application";
-        desktopName = "Stardust XR";
-      }).overrideAttrs (oldAttrs: {
-        passthru = (oldAttrs.passthru or {}) // {
-          providedSessions = [ "stardust-xr" ];
-        };
-      })
-    )
-    (
-      (pkgs.makeDesktopItem {
-        destination = "/share/wayland-sessions";
-        name = "kmscon";
-        comment = "TODO";
-        exec = lib.getExe pkgs.kmscon;
-        type = "Application";
-        desktopName = "kmscon";
-      }).overrideAttrs (oldAttrs: {
-        passthru = (oldAttrs.passthru or {}) // {
-          providedSessions = [ "kmscon" ];
-        };
-      })
-    )
-  ];
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors = {
+      stardustxr = {
+        prettyName = "Stardust XR";
+        comment = "TODO stardust-xr";
+        binPath = lib.getExe pkgs.stardust-xr-server;
+      };
+      kmscon = {
+        prettyName = "kmscon";
+        comment = "TODO kmscon";
+        binPath = lib.getExe pkgs.kmscon;
+      };
+    };
+  };
 }
