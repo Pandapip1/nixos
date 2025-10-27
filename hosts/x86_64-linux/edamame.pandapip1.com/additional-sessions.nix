b304b41971c606e74a9ddce131451440f7e6ede2
Gavin John <gavinnjohn@gmail.com>
2025-10-26 20:03:32 -0700

    Properly declare additional sessions

--- hosts/x86_64-linux/edamame.pandapip1.com/additional-sessions.nix
+++ hosts/x86_64-linux/edamame.pandapip1.com/additional-sessions.nix
@@ -5,21 +5,33 @@
 
 {
   services.displayManager.sessionPackages = [
-    (pkgs.makeDesktopItem {
-      destination = "/share/wayland-sessions";
-      name = "Stardust XR";
-      comment = "TODO";
-      exec = lib.getExe pkgs.stardust-xr-server;
-      type = "Application";
-      desktopName = "Stardust XR";
-    })
-    (pkgs.makeDesktopItem {
-      destination = "/share/wayland-sessions";
-      name = "kmscon";
-      comment = "TODO";
-      exec = lib.getExe pkgs.kmscon;
-      type = "Application";
-      desktopName = "kmscon";
-    })
+    (
+      (pkgs.makeDesktopItem {
+        destination = "/share/wayland-sessions";
+        name = "stardust-xr";
+        comment = "TODO";
+        exec = lib.getExe pkgs.stardust-xr-server;
+        type = "Application";
+        desktopName = "Stardust XR";
+      }).overrideAttrs (oldAttrs: {
+        passthru = (oldAttrs.passthru or {}) // {
+          providedSessions = [ "stardust-xr" ];
+        };
+      })
+    )
+    (
+      (pkgs.makeDesktopItem {
+        destination = "/share/wayland-sessions";
+        name = "kmscon";
+        comment = "TODO";
+        exec = lib.getExe pkgs.kmscon;
+        type = "Application";
+        desktopName = "kmscon";
+      }).overrideAttrs (oldAttrs: {
+        passthru = (oldAttrs.passthru or {}) // {
+          providedSessions = [ "kmscon" ];
+        };
+      })
+    )
   ];
 }
