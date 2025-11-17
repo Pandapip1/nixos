2b9c43888558706273520d49320f8985a868db77
Gavin John <gavinnjohn@gmail.com>
2025-11-17 01:41:02 -0800

    Make nix-gc system config limit configurable

--- modules/services/nix-gc.nix
+++ modules/services/nix-gc.nix
@@ -1,20 +1,26 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }:
 
 {
-  systemd.services.nix-gc = let
-    configurationLimit = 16;
-  in lib.mkForce {
+  options.nix-gc.configurationLimit = lib.mkOption {
+    type = lib.types.int;
+    default = 16;
+    description = "Number of generations to keep when running nix-gc";
+  };
+
+  systemd.services.nix-gc = lib.mkForce {
     description = "Nix Garbage Collector";
     script = ''
-      ${lib.getExe' pkgs.nix "nix-env"} --delete-generations +${toString configurationLimit} --profile /nix/var/nix/profiles/system
+      ${lib.getExe' pkgs.nix "nix-env"} --delete-generations +${toString config.nix-gc.configurationLimit} --profile /nix/var/nix/profiles/system
       exec ${lib.getExe' pkgs.nix "nix-collect-garbage"}
     '';
     serviceConfig.Type = "oneshot";
     startAt = "daily";
   };
+
   systemd.timers.nix-gc.timerConfig.Persistent = lib.mkForce true;
 }
