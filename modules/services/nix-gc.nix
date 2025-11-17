1fb12d546f941458a6e8b5d1eeb6b2426112d486
Gavin John <gavinnjohn@gmail.com>
2025-11-17 01:44:13 -0800

    Fix gc

--- modules/services/nix-gc.nix
+++ modules/services/nix-gc.nix
@@ -12,15 +12,17 @@
     description = "Number of generations to keep when running nix-gc";
   };
 
-  systemd.services.nix-gc = lib.mkForce {
-    description = "Nix Garbage Collector";
-    script = ''
-      ${lib.getExe' pkgs.nix "nix-env"} --delete-generations +${toString config.nix-gc.configurationLimit} --profile /nix/var/nix/profiles/system
-      exec ${lib.getExe' pkgs.nix "nix-collect-garbage"}
-    '';
-    serviceConfig.Type = "oneshot";
-    startAt = "daily";
-  };
+  config = {
+    systemd.services.nix-gc = lib.mkForce {
+      description = "Nix Garbage Collector";
+      script = ''
+        ${lib.getExe' pkgs.nix "nix-env"} --delete-generations +${toString config.nix-gc.configurationLimit} --profile /nix/var/nix/profiles/system
+        exec ${lib.getExe' pkgs.nix "nix-collect-garbage"}
+      '';
+      serviceConfig.Type = "oneshot";
+      startAt = "daily";
+    };
 
-  systemd.timers.nix-gc.timerConfig.Persistent = lib.mkForce true;
+    systemd.timers.nix-gc.timerConfig.Persistent = lib.mkForce true;
+  };
 }
