b6abefc331c8b7bacf66b4713934b4f3c77cc274
Gavin John <gavinnjohn@gmail.com>
2025-11-18 17:03:43 -0800

    Fix typo in nebula

--- modules/services/nebula.nix
+++ modules/services/nebula.nix
@@ -12,7 +12,10 @@ in
   services.nebula.networks.nebula0 = {
     enable = lib.mkDefault false;
     ca = "${self}/global_config/nebula/ca.crt";
-    cert = "${self}/hosts/${config.nixpkgs.hostPlatform.system}/${config.networking.hostname}.${config.networking.domain}/config/nebula/${hostname}.crt";
+    cert = let
+      fqdn = if (config.networking.domain == null) then config.networking.hostName else "${config.networking.hostName}.${config.networking.domain}";
+    in
+      "${self}/hosts/${config.nixpkgs.hostPlatform.system}/${fqdn}/config/nebula/${hostname}.crt";
     key = "/etc/secrets/nebula/${hostname}.key";
     staticHostMap = {
       "10.0.0.1" = [
