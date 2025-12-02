5aaa96705ff580d58abd7ab02a51cea9e29295df
Gavin John <gavinnjohn@gmail.com>
2025-12-01 23:17:02 -0800

    Systemwide pipewire. Ship it.

--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -13,8 +13,13 @@
     pulse.enable = true;
     jack.enable = true;
     wireplumber.enable = true;
-    # systemWide = true; # Not the default and not recommended but makes the most sense to me.
+    systemWide = true; # Not the default and not recommended but makes the most sense to me.
   };
+  # Update the socket groups so pipewire doesn't have to be added to every user
+  systemd.user.sockets.pipewire.socketConfig.SocketGroup = "audio";
+  systemd.user.sockets.pipewire-pulse.socketConfig.SocketGroup = "audio";
+  systemd.sockets.pipewire.socketConfig.SocketGroup = "audio";
+  systemd.sockets.pipewire-pulse.socketConfig.SocketGroup = "audio";
 
   # Required for system-wide audio to work for normal users
   /* users.commonGroups.normal = [
