551633023fc47bbca1c091fadeedcaae250fd4f3
Gavin John <gavinnjohn@gmail.com>
2025-12-02 01:18:51 -0800

    Switch to RT kernel

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -1,11 +1,12 @@
 {
+  lib,
   pkgs,
   ...
 }:
 
 {
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest_rt; # RT is a performance hit but I really like the fact that it's more consistent
     initrd.systemd.enable = true;
   };
 }
