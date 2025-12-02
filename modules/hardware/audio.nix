d6369e9f605d560a0e3a8dc0a60681b80918c1e3
Gavin John <gavinnjohn@gmail.com>
2025-12-01 23:40:13 -0800

    Disable audio stuttering and node suspension

--- modules/hardware/audio.nix
+++ modules/hardware/audio.nix
@@ -12,7 +12,51 @@
     alsa.support32Bit = pkgs.stdenvNoCC.hostPlatform.isx86_64;
     pulse.enable = true;
     jack.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber/wireplumber.conf.d/disable-suspension.conf" ''
+          monitor.alsa.rules = [
+            {
+              matches = [
+                {
+                  # Matches all sources
+                  node.name = "~alsa_input.*"
+                },
+                {
+                  # Matches all sinks
+                  node.name = "~alsa_output.*"
+                }
+              ]
+              actions = {
+                update-props = {
+                  session.suspend-timeout-seconds = 0
+                }
+              }
+            }
+          ]
+          monitor.bluez.rules = [
+            {
+              matches = [
+                {
+                  # Matches all sources
+                  node.name = "~bluez_input.*"
+                },
+                {
+                  # Matches all sinks
+                  node.name = "~bluez_output.*"
+                }
+              ]
+              actions = {
+                update-props = {
+                  session.suspend-timeout-seconds = 0
+                }
+              }
+            }
+          ]
+        '')
+      ]
+    };
     systemWide = true; # Not the default and not recommended but makes the most sense to me.
   };
   # Update the socket groups so pipewire doesn't have to be added to every user
