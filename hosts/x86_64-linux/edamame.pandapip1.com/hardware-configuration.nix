572fd39ce1b4ac7d5efbbca8e67aa419acaa2200
Gavin John <gavinnjohn@gmail.com>
2025-12-23 16:46:43 -0500

    Edamame fsconfig

--- hosts/x86_64-linux/edamame.pandapip1.com/hardware-configuration.nix
+++ hosts/x86_64-linux/edamame.pandapip1.com/hardware-configuration.nix
@@ -1,3 +1,34 @@
 {
-  boot.loader.grub.device = "/dev/TODO";
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
+
+{
+  # GRUB, because I prefer GRUB to systemd-boot
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      efiSupport = true;
+      devices = lib.mkForce [ "/dev/disk/by-id/TODO" ];
+      memtest86.enable = true;
+    };
+  };
+
+  # Kernel stuff
+  boot.kernelParams = [ ];
+  boot.initrd.availableKernelModules = [
+    # TODO
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [
+    # TODO
+  ];
+  boot.extraModulePackages = [ ];
+
+  # Use disko for filesystem management
+  imports = [ ./disko-config.nix ];
 }
