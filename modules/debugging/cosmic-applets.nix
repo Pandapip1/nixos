36ac32ca04daf0d821773a5dd38c84851b0b8523
Gavin John <gavinnjohn@gmail.com>
2025-12-31 13:18:41 -0500

    Apparently you still can't override cargoHash

--- modules/debugging/cosmic-applets.nix
+++ modules/debugging/cosmic-applets.nix
@@ -9,14 +9,56 @@ in
 {
   nixpkgs.overlays = [
     (_: prev: {
-      cosmic-applets = prev.cosmic-applets.overrideAttrs {
+      cosmic-applets = with prev; rustPlatform.buildRustPackage (finalAttrs: {
+        pname = "cosmic-applets";
+        version = "1.0.0-fixed-icons";
+
+        # nixpkgs-update: no auto update
         src = pkgs.fetchFromGitHub {
           owner = "Pandapip1";
           repo = "cosmic-applets";
           rev = "14deb0d4aad0b8b210f52c48d3b7486f4ff725e1";
           hash = "sha256-KRpaevv4DF32CoZNfhSs6u9WPYNB3n01qpLiZy+CFbU=";
         };
-      };
+
+        cargoHash = "sha256-Eq0RSA8TYHKNRx5mg010iyrONigKR0GgGZ3fXnWOmG8=";
+
+        nativeBuildInputs = [
+          just
+          pkg-config
+          util-linuxMinimal
+          libcosmicAppHook
+          rustPlatform.bindgenHook
+        ];
+
+        buildInputs = [
+          dbus
+          glib
+          libinput
+          pulseaudio
+          pipewire
+          udev
+        ];
+
+        dontUseJustBuild = true;
+        dontUseJustCheck = true;
+
+        justFlags = [
+          "--set"
+          "prefix"
+          (placeholder "out")
+          "--set"
+          "target"
+          "${stdenv.hostPlatform.rust.cargoShortTarget}/release"
+        ];
+
+        preFixup = ''
+          libcosmicAppWrapperArgs+=(
+            --set-default X11_BASE_RULES_XML ${xkeyboard_config}/share/X11/xkb/rules/base.xml
+            --set-default X11_EXTRA_RULES_XML ${xkeyboard_config}/share/X11/xkb/rules/base.extras.xml
+          )
+        '';
+      });
     })
   ];
 }
