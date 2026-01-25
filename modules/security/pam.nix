a56f32f21164d0d608083fc04d96280563861ede
Gavin John <gavinnjohn@gmail.com>
2026-01-24 20:55:54 -0800

    And comment it out

--- modules/security/pam.nix
+++ modules/security/pam.nix
@@ -4,27 +4,27 @@
   ...
 }:
 let
-  pam_ropc =
-    with pkgs;
-    rustPlatform.buildRustPackage (finalAttrs: {
-      pname = "pam-ropc";
-      version = "nightly";
+  # pam_ropc =
+  #   with pkgs;
+  #   rustPlatform.buildRustPackage (finalAttrs: {
+  #     pname = "pam-ropc";
+  #     version = "nightly";
 
-      src = self.outPath;
+  #     src = self.outPath;
 
-      cargoLock = {
-        lockFile = finalAttrs.src + "/Cargo.lock";
-      };
+  #     cargoLock = {
+  #       lockFile = finalAttrs.src + "/Cargo.lock";
+  #     };
 
-      buildInputs = [
-        pam
-      ];
+  #     buildInputs = [
+  #       pam
+  #     ];
 
-      postInstall = ''
-        mkdir -p $out/lib/security
-        ln -s $out/lib/libpam_ropc.so $out/lib/security/pam_ropc.so
-      '';
-    });
+  #     postInstall = ''
+  #       mkdir -p $out/lib/security
+  #       ln -s $out/lib/libpam_ropc.so $out/lib/security/pam_ropc.so
+  #     '';
+  #   });
 in
 {
 
