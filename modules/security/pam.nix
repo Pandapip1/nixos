9433722ce39845ca91fee8d682e6f1f7a81b1ce5
Gavin John <gavinnjohn@gmail.com>
2026-01-24 20:55:34 -0800

    Add initial pam stuff

---
+++ modules/security/pam.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+let
+  pam_ropc =
+    with pkgs;
+    rustPlatform.buildRustPackage (finalAttrs: {
+      pname = "pam-ropc";
+      version = "nightly";
+
+      src = self.outPath;
+
+      cargoLock = {
+        lockFile = finalAttrs.src + "/Cargo.lock";
+      };
+
+      buildInputs = [
+        pam
+      ];
+
+      postInstall = ''
+        mkdir -p $out/lib/security
+        ln -s $out/lib/libpam_ropc.so $out/lib/security/pam_ropc.so
+      '';
+    });
+in
+{
+
+}
