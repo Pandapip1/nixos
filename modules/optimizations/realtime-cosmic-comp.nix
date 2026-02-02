4e23b4d08443abdc73096d39807ee4fb30ca4227
Gavin John <gavinnjohn@gmail.com>
2026-02-02 14:32:07 -0800

    More fixes yay

--- modules/optimizations/realtime-cosmic-comp.nix
+++ modules/optimizations/realtime-cosmic-comp.nix
@@ -1,10 +1,11 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }:
 
-{
+lib.mkIf config.services.desktopManager.cosmic.enable {
   systemd.user.services.cosmic-comp-rt = {
     description = "Promote cosmic-comp to SCHED_RR via rtkit";
     wantedBy = [ "graphical-session.target" ];
@@ -16,11 +17,17 @@
         ${lib.getExe pkgs.bash} -lc '
           BIN_NAME=".cosmic-comp-wrapped"
           echo "Waiting for $BIN_NAME..."
-          while ! pgrep -x $BIN_NAME >/dev/null; do
-            sleep 0.05
+          while ! pgrep -x "$BIN_NAME" >/dev/null; do
+            sleep 1
           done
 
-          PID=$(pgrep -x $BIN_NAME | head -n1)
+          PID=$(pgrep -n -x "$BIN_NAME")
+
+          if chrt -p "$PID" 2>/dev/null | grep -q 'scheduling policy: SCHED_RR'; then
+            echo "$BIN_NAME already SCHED_RR; done."
+            exit 0
+          fi
+
           echo "Promoting $BIN_NAME (PID=$PID) to SCHED_RR"
 
           ${lib.getExe' pkgs.rtkit "rtkitctl"} \
