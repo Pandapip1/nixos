34d0031884999260b40271018b48bb003cdeefa9
Gavin John <gavinnjohn@gmail.com>
2026-02-08 01:07:21 -0800

    Enable unattended upgrades

---
+++ modules/security/untattended-upgrade.nix
@@ -0,0 +1,14 @@
+{
+  self,
+  ...
+}:
+
+{
+  system.autoUpgrade = {
+    enable = true;
+    flake = self.outPath;
+    flags = [ "--print-build-logs" ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  }
+}
