d04536d369ed6728d0625322ab9bd6798da233ab
Gavin John <gavinnjohn@gmail.com>
2026-03-31 22:42:07 -0700

    Add a bunch of packages to deli

--- hosts/x86_64-linux/deli.pandapip1.com/package.nix
+++ hosts/x86_64-linux/deli.pandapip1.com/package.nix
@@ -1,4 +1,9 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   imports = [
     ./hardware-configuration.nix
   ];
@@ -6,6 +11,24 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
   services.gnome.gnome-keyring.enable = false; # For some reason enabling cosmic enables gnome keyring. I want to use keepassxc thank you very much.
+
+  programs.opengamepadui = {
+    enable = true;
+    inputplumber.enable = true;
+    powerstation.enable = true;
+    gamescopeSession.enable = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    extest.enable = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    lutris
+    prismlauncher
+  ];
 
   system.stateVersion = "25.11";
 }
