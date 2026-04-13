0990e77ebc867506b5f987588157d590fdb90c35
Gavin John <gavinnjohn@gmail.com>
2026-04-12 17:43:46 -0700

    pam_ropc

---
+++ modules/packages_to_upstream/pam-ropc.nix
@@ -0,0 +1,32 @@
+
+{
+  nixpkgs.overlays = [
+    (_: prev: {
+      pam_ropc = prev.rustPlatform.buildRustPackage (finalAttrs: {
+        pname = "pam_ropc";
+        version = "0-unstable-TODO";
+
+        src = prev.fetchFromCodeberg {
+          owner = "pandapip1";
+          repo = "pam-ropc";
+          rev = "2155afe3815f3bb13ff83bdf6736bd9749fffb92";
+        };
+
+        cargoLock = {
+          lockFile = finalAttrs.src + "/Cargo.lock";
+          allowBuiltinFetchGit = true;
+        };
+            
+        buildInputs = with prev; [
+          libsodium
+          pam
+        ];
+
+        postInstall = ''
+          mkdir -p $out/lib/security
+          ln -s $out/lib/libpam_ropc.so $out/lib/security/pam_ropc.so
+        '';
+      });
+    })
+  ];
+}
--- modules/security/pam.nix
+++ modules/security/pam.nix
@@ -3,29 +3,16 @@
   pkgs,
   ...
 }:
-let
-  # pam_ropc =
-  #   with pkgs;
-  #   rustPlatform.buildRustPackage (finalAttrs: {
-  #     pname = "pam-ropc";
-  #     version = "nightly";
 
-  #     src = self.outPath;
-
-  #     cargoLock = {
-  #       lockFile = finalAttrs.src + "/Cargo.lock";
-  #     };
-
-  #     buildInputs = [
-  #       pam
-  #     ];
-
-  #     postInstall = ''
-  #       mkdir -p $out/lib/security
-  #       ln -s $out/lib/libpam_ropc.so $out/lib/security/pam_ropc.so
-  #     '';
-  #   });
-in
 {
-
+  security.pam.services.login.rules.auth.ropc = {
+    control = "sufficient";
+    modulePath = "${pkgs.pam_ropc}/lib/security/pam_ropc.so";
+    order = config.security.pam.services.login.rules.auth.unix.order + 10; # Place it just after the built-in unix rule
+    settings = {
+      client_id = "pam_ropc";
+      token_url = "https://keycloak.berry.pandapip1.com/realms/pandaport/protocol/openid-connect/token";
+      timeout_ms = 1000;
+    };
+  };
 }
