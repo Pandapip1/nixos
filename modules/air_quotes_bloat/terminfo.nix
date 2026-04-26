46385d5245918c7c0c558a2333a15fc2a801a322
Gavin John <gavinnjohn@gmail.com>
2026-04-26 15:33:31 -0700

    Add kitty terminfo wrapper

---
+++ modules/air_quotes_bloat/terminfo.nix
@@ -0,0 +1,29 @@
+{
+  environment.systemPackages = let
+    mkTerminfoWrapper = package: stdenvNoCC.mkDerivation {
+      name = "${package.name}-terminfo";
+      inherit (package) version;
+
+      src = package;
+
+      dontConfigure = true;
+      dontBuild = true;
+
+      # cp used instead of ln so that the original package can be discarded
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out/share
+
+        if [ -d "${package}/share/terminfo" ]; then
+          cp -r "${package}/share/terminfo" $out/share/
+        else
+          echo "No terminfo found in ${package}" >&2
+          exit 1
+        fi
+
+        runHook postInstall
+      '';
+    };
+  in builtins.map mkTerminfoWrapper (with pkgs; [ kitty ]);
+}
