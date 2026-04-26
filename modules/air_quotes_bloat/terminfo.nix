7d07e9506c78a9550682d387614e1e97ef9973dd
Gavin John <gavinnjohn@gmail.com>
2026-04-26 15:45:07 -0700

    Enable all terminfo

--- modules/air_quotes_bloat/terminfo.nix
+++ modules/air_quotes_bloat/terminfo.nix
@@ -4,31 +4,5 @@
 }:
 
 {
-  environment.systemPackages = let
-    mkTerminfoWrapper = package: pkgs.stdenvNoCC.mkDerivation {
-      name = "${package.name}-terminfo";
-      inherit (package) version;
-
-      src = package;
-
-      dontConfigure = true;
-      dontBuild = true;
-
-      # cp used instead of ln so that the original package can be discarded
-      installPhase = ''
-        runHook preInstall
-
-        mkdir -p $out/share
-
-        if [ -d "share/terminfo" ]; then
-          cp -r "share/terminfo" $out/share/
-        else
-          echo "No terminfo found in share/terminfo" >&2
-          exit 1
-        fi
-
-        runHook postInstall
-      '';
-    };
-  in builtins.map mkTerminfoWrapper (with pkgs; [ kitty ]);
+  environment.enableAllTerminfo = true;
 }
