7b18267943f87307b860575f481791fd3a0dba5a
Gavin John <gavinnjohn@gmail.com>
2026-04-30 22:49:48 -0700

    Fix that too

--- modules/debugging/plover.nix
+++ modules/debugging/plover.nix
@@ -9,7 +9,7 @@ in
 {
   nixpkgs.overlays = [
     (_: prev: {
-      plover_5 = plover_5.overrideAttrs (oldAttrs: {
+      plover_5 = prev.plover_5.overrideAttrs (oldAttrs: {
         patches = (oldAttrs.patches or []) ++ prev.fetchPatch {
           url = "https://github.com/opensteno/plover/commit/b6453858c4bb99ae91ac936074ffec8924a37eac.patch";
         };
