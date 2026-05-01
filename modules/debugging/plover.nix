07c28fdf9d74de3ca2d92ca168521e496d4de7ba
Gavin John <gavinnjohn@gmail.com>
2026-05-01 00:32:00 -0700

    Some more plover 'fixes'

--- modules/debugging/plover.nix
+++ modules/debugging/plover.nix
@@ -9,23 +9,29 @@ in
 {
   nixpkgs.overlays = [
     (_: prev: {
-      plover_5 = prev.plover_5.overrideAttrs (oldAttrs: {
-        patches = (oldAttrs.patches or []) ++ [
-          (prev.fetchpatch {
-            url = "https://github.com/opensteno/plover/commit/b6453858c4bb99ae91ac936074ffec8924a37eac.patch";
-            hash = "sha256-5AYQJdwmCDfx22he0RrLpnwkv9Bs7Yw3HHxJIYJqzUg=";
-          })
-        ];
+      plover_5 = prev.plover_5.overridePythonAttrs (oldAttrs: {
+        verison = "5.3.0";
 
-        postPatch = (prev.postPatch or "") + ''
+        src = oldAttrs.src.overrideAttrs {
+          rev = "eab1dde8c0283da77603161ead170d4cfa982fe0";
+          hash = "sha256-0xes3Tgs39QSUQzV73Ouv7FQIst2u+xIc5X65WkGBkI=";
+        };
+
+        postPatch = (oldAttrs.postPatch or "") + ''
           substituteInPlace setup.cfg \
             --replace-fail \
-              "plover              = plover.scripts.main:main" \
-              "plover              = plover.scripts.dist_main:main" \
+              "plover.scripts.main:main" \
+              "plover.scripts.dist_main:main" \
             --replace-fail \
-              "plover_plugins      = plover.plugins_manager.__main__:main" \
-              "plover_plugins      = plover.scripts.dist_plugins_manager:main"
+              "plover.plugins_manager.__main__:main" \
+              "plover.scripts.dist_plugins_manager:main"
+          sed -i 's/xkbcommon<1.1/xkbcommon/g' reqs/dist.txt
         '';
+
+        dependencies = oldAttrs.dependencies ++ (with prev.python3Packages; [
+          hidapi
+          xkbcommon
+        ]);
       });
     })
   ];
