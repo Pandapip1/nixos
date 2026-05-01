b46e1ff144ad72dc6d9d501e7454eb1326aba726
Gavin John <gavinnjohn@gmail.com>
2026-04-30 22:49:30 -0700

    Fix missing ;

--- modules/debugging/plover.nix
+++ modules/debugging/plover.nix
@@ -13,7 +13,7 @@ in
         patches = (oldAttrs.patches or []) ++ prev.fetchPatch {
           url = "https://github.com/opensteno/plover/commit/b6453858c4bb99ae91ac936074ffec8924a37eac.patch";
         };
-      })
+      });
     })
   ];
 }
