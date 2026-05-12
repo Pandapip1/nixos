3823b45c0607fac0252cadcc2200831adcd131e4
Gavin John <gavinnjohn@gmail.com>
2026-05-12 12:28:25 -0700

    grape is now my main "publicly accessible stuff" server

---
+++ hosts/x86_64-linux/grape.pandapip1.com/nginx.nix
@@ -0,0 +1,101 @@
+
+{
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "gavinnjohn@gmail.com";
+    certs = {
+      "pandapip1.com" = {
+        dnsProvider = "cloudflare";
+        environmentFile = "/etc/secrets/acme/cloudflare-api-key";
+      };
+      "grape.pandapip1.com" = {
+        dnsProvider = "cloudflare";
+        environmentFile = "/etc/secrets/acme/cloudflare-api-key";
+        extraDomainNames = [ "*.grape.pandapip1.com" ];
+      };
+      "cache.pandapip1.com" = {
+        dnsProvider = "cloudflare";
+        environmentFile = "/etc/secrets/acme/cloudflare-api-key";
+        extraDomainNames = [ "*.cache.pandapip1.com" ];
+      };
+    };
+  };
+
+  secrets.acme.ownership.user = "acme";
+
+  services.nginx = {
+    enable = true;
+
+    # Enable recommended settings
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    # Only allow PFS-enabled ciphers with AES256
+    sslCiphers = "AES256+EECDH:AES256+EDH:!aNULL";
+
+    # HTTPS hardening
+    appendHttpConfig = ''
+      # Add HSTS header with preloading to HTTPS requests.
+      # Adding this header to HTTP requests is discouraged
+      map $scheme $hsts_header {
+        https   "max-age=31536000; includeSubdomains; preload";
+      }
+      add_header Strict-Transport-Security $hsts_header;
+
+      # Enable CSP for your services.
+      #add_header Content-Security-Policy "script-src 'self'; object-src 'none'; base-uri 'none';" always;
+
+      # Minimize information leaked to other domains
+      add_header 'Referrer-Policy' 'origin-when-cross-origin';
+
+      # Disable embedding as a frame
+      # Why is nginx like this? This is so stupid
+      map $server_name $x_frame_options {
+        default "DENY";
+        # keycloak.berry.pandapip1.com "SAMEORIGIN";
+      }
+      add_header X-Frame-Options $x_frame_options;
+
+      # Prevent injection of code in other mime types (XSS Attacks)
+      add_header X-Content-Type-Options nosniff;
+
+      # Set cookie security
+      proxy_cookie_flags ~ KEYCLOAK_SESSION Secure SameSite=None;
+      proxy_cookie_flags ~ KEYCLOAK_IDENTITY Secure SameSite=None;
+    '';
+
+    virtualHosts = {
+      "grape.pandapip1.com" = {
+        useACMEHost = "grape.pandapip1.com";
+        forceSSL = true;
+        root = ./config/static/grape.pandapip1.com;
+      };
+      # Pandapip1.com homepage
+      "pandapip1.com" = {
+        useACMEHost = "pandapip1.com";
+        forceSSL = true;
+        root = ./config/static/pandapip1.com;
+      };
+      # Binary caches
+      "cache.pandapip1.com" = {
+        useACMEHost = "cache.pandapip1.com";
+        forceSSL = true;
+        root = ./config/static/cache.pandapip1.com;
+      };
+      # Default: reject all requests to unknown vhosts
+      # Unfortunately no easy way to do this with just a single flag, hence this nonsense
+      default = {
+        default = true;
+        serverName = "_";
+        useACMEHost = "grape.pandapip1.com";
+        addSSL = true;
+        extraConfig = "ssl_reject_handshake on;";
+        locations."/" = {
+          return = "444";
+        };
+      };
+    };
+  };
+}
