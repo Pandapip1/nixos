798ea0f83f4545e9fd693625562a3b0fe00ab956
Gavin John <gavinnjohn@gmail.com>
2026-05-12 12:38:26 -0700

    Add attic cache

---
+++ hosts/x86_64-linux/grape.pandapip1.com/attic.nix
@@ -0,0 +1,27 @@
+{  
+  services = {
+    atticd = {
+      enable = true;
+      settings = {
+        listen = "127.0.0.1:16206";
+        api-endpoint = "https://cache.nixos.pandapip1.com/";
+        storage = {
+          type = "s3";
+          bucket = "a7310486287908d892e70d10";
+          endpoint = "https://s3.us-east-005.backblazeb2.com";
+        };
+        compression.type = "xz";
+      };
+      environmentFile = "/env/secrets/attic/.env";
+    };
+  };
+  services.nginx = {
+    virtualHosts = {
+      "nixos.cache.pandapip1.com" = {
+        useACMEHost = "cache.pandapip1.com";
+        forceSSL = true;
+        locations."/".proxyPass = "http://127.0.0.1:16206";
+      };
+    };
+  };
+}
