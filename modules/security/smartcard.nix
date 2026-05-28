58c1564562d378596dbaf598b2594bb68c85b56c
Gavin John <gavinnjohn@gmail.com>
2026-05-28 14:32:29 -0700

    Smartcard support

--- modules/security/smartcard.nix
+++ modules/security/smartcard.nix
@@ -1,6 +1,15 @@
 {
+  pkgs,
+  ...
+}:
+
+{
   hardware.gpgSmartcards.enable = true;
-  # services.pcscd.enable = true; # Use internal CCID instead
+  services.pcscd.enable = true;
+  hardware.nfc-nci.enable = true;
+  environment.systemPackages = with pkgs; [
+    global-platform-pro
+  ];
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
