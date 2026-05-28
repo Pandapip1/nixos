50b0a6c232a1ba5e5dfebbef3b22f0554ae01048
Gavin John <gavinnjohn@gmail.com>
2026-05-28 14:57:51 -0700

    That rebuilds the world oops

--- modules/debugging/pcscd-debug.nix
+++
@@ -1,8 +0,0 @@
-{
-  nixpkgs.overlays = [
-    (final: prev: {
-      pcsclite = prev.enableDebugging prev.pcsclite;
-      pcscliteWithPolkit = prev.enableDebugging prev.pcscliteWithPolkit;
-    })
-  ];
-}
