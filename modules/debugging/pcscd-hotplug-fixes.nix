94d24549222e5b2f6c4a53778555ff1c9c11b35b
Gavin John <gavinnjohn@gmail.com>
2026-06-04 16:12:02 -0700

    Never mind those cause mass rebuilds

---
+++ modules/debugging/pcscd-hotplug-fixes.nix
@@ -0,0 +1,21 @@
+{
+  nixpkgs.overlays = [
+    (final: prev: {
+      # ccid = prev.ccid.overrideAttrs (old: {
+      #   src = final.fetchFromGitHub {
+      #     owner = "LudovicRousseau";
+      #     repo = "CCID";
+      #     rev = "8d7e34c704a82f3c7c7ee02f8ba496fd2b2b0c33";
+      #   };
+      # });
+
+      # pcsclite = prev.pcsclite.overrideAttrs (old: {
+      #   src = final.fetchFromGitHub {
+      #     owner = "LudovicRousseau";
+      #     repo = "PCSC";
+      #     rev = "c4a2e9900abc9a0d9bee5f025edc7d9c44820ddb";
+      #   };
+      # });
+    })
+  ];
+}
