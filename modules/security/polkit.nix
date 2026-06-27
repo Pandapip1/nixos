dc8bdcab456e6787d378e91b2851f5177a9bd717
Gavin John <gavinnjohn@gmail.com>
2026-06-27 12:06:25 -0400

    oops

--- modules/security/polkit.nix
+++ modules/security/polkit.nix
@@ -4,9 +4,10 @@
   ...
 }:
 
-lib.mkIf (config.services.graphical-desktop.enable && !services.desktopManager.cosmic.enable) {
-  # Use soteria as our polkit agent
-  # Seemingly no tty-based polkit agents (sort of makes sense)
-  # COSMIC has cosmic-osd, we'll use that
-  security.soteria.enable = true;
-}
+lib.mkIf (config.services.graphical-desktop.enable && !config.services.desktopManager.cosmic.enable)
+  {
+    # Use soteria as our polkit agent
+    # Seemingly no tty-based polkit agents (sort of makes sense)
+    # COSMIC has cosmic-osd, we'll use that
+    security.soteria.enable = true;
+  }
