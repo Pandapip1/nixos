01997801599fc92fb5073ba3ef2acaa8604adc85
Gavin John <gavinnjohn@gmail.com>
2026-06-27 12:37:46 -0400

    Guess no more coral tpu for me anymore

--- modules/hardware/misc/coral-tpu.nix
+++ modules/hardware/misc/coral-tpu.nix
@@ -1,6 +1,7 @@
 {
-  hardware.coral = {
-    usb.enable = true;
-    pcie.enable = true;
-  };
+  # Broken on 7.1.1 after gasket_mm_unmap_region renamed to zap_special_vma_range
+  # hardware.coral = {
+  #   usb.enable = true;
+  #   pcie.enable = true;
+  # };
 }
