041c168023b1ebddde090ae8aafab3bdd45a0b87
Gavin John <gavinnjohn@gmail.com>
2026-07-14 22:15:46 -0400

    Enable monado on apple

--- hosts/x86_64-linux/apple.pandapip1.com/package.nix
+++ hosts/x86_64-linux/apple.pandapip1.com/package.nix
@@ -112,6 +112,17 @@
 
   services.logind.lidSwitch = "ignore";
 
+  # Monado
+  services.monado = {
+    enable = true;
+    defaultRuntime = true;
+  };
+
+  # Motoc
+  environment.systemPackages = with pkgs; [
+    motoc
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
