3d9b365c5f9fc9ed3d7b95907b8e221e9ca31ec9
Gavin John <gavinnjohn@gmail.com>
2026-07-17 20:11:29 -0400

    Update kernel patches

--- modules/patches/kernel/kernel-patches.nix
+++ modules/patches/kernel/kernel-patches.nix
@@ -1,8 +1,12 @@
 {
   boot.kernelPatches = [
     {
-      name = "non-desktop-override";
-      patch = ./0001-drm-add-sysfs-non_desktop_override-for-connectors.patch;
+      name = "resolution-quirk-match";
+      patch = ./0001-drm-edid-match-on-active-resolution.patch;
+    }
+    {
+      name = "xreal-light-non-desktop";
+      patch = ./0002-drm-edid-add-xreal-air-non-desktop-quirk-in-SBS.patch;
     }
   ];
 }
