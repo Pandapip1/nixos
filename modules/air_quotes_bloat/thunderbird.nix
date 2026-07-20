55cfea96922eea88df220a3979cb0b6dec2716b4
Gavin John <gavinnjohn@gmail.com>
2026-07-20 19:16:21 -0400

    Un-disable it

--- modules/air_quotes_bloat/thunderbird.nix
+++ modules/air_quotes_bloat/thunderbird.nix
@@ -5,7 +5,9 @@
 }:
 
 {
-  # programs.thunderbird = {
-  #   enable = lib.mkDefault (config.services.graphical-desktop.enable && !(config.optimizations.lean.enable));
-  # };
+  programs.thunderbird = {
+    enable = lib.mkDefault (
+      config.services.graphical-desktop.enable && !(config.optimizations.lean.enable)
+    );
+  };
 }
