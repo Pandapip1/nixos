9b6db4dd4b934478ddd478794fe088523ce67099
Gavin John <gavinnjohn@gmail.com>
2026-07-22 19:27:24 -0400

    Temporarily disable my precious kernel patches

--- modules/patches/kernel/kernel-patches.nix
+++ modules/patches/kernel/kernel-patches.nix
@@ -1,12 +1,12 @@
 {
   boot.kernelPatches = [
-    {
-      name = "resolution-quirk-match";
-      patch = ./0001-drm-edid-match-on-active-resolution.patch;
-    }
-    {
-      name = "xreal-light-non-desktop";
-      patch = ./0002-drm-edid-add-xreal-air-non-desktop-quirk-in-SBS.patch;
-    }
+    # {
+    #   name = "resolution-quirk-match";
+    #   patch = ./0001-drm-edid-match-on-active-resolution.patch;
+    # }
+    # {
+    #   name = "xreal-light-non-desktop";
+    #   patch = ./0002-drm-edid-add-xreal-air-non-desktop-quirk-in-SBS.patch;
+    # }
   ];
 }
