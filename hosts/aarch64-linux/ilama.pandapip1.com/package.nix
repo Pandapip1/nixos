970b019dae525e92d0af2b03b19c52b7cf4add25
Gavin John <gavinnjohn@gmail.com>
2026-07-24 13:28:59 -0400

    Emabe COSMIC DE

--- hosts/aarch64-linux/ilama.pandapip1.com/package.nix
+++ hosts/aarch64-linux/ilama.pandapip1.com/package.nix
@@ -1,5 +1,10 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
+  # Use COSMIC DE
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
+  services.gnome.gnome-keyring.enable = false; # For some reason enabling cosmic enables gnome keyring. I want to use keepassxc thank you very much.
+
   system.stateVersion = "26.11";
 }
