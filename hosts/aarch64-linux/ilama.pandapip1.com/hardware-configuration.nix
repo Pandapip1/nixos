5ead9fbb717f55127a7b6d29eb1af6f2f7617be4
Gavin John <gavinnjohn@gmail.com>
2026-07-24 13:23:51 -0400

    Let's quickly do that

--- hosts/aarch64-linux/ilama.pandapip1.com/hardware-configuration.nix
+++ hosts/aarch64-linux/ilama.pandapip1.com/hardware-configuration.nix
@@ -1,5 +1,6 @@
 {
   hardware.asahi.enable = true;
+  hardware.firmware.peripheralFirmwareDirectory = ./vendorfw;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
