7e12b43b4e831e76bed0da6615f4465ae3582f4d
Gavin John <gavinnjohn@gmail.com>
2026-07-29 10:41:55 -0400

    Yea duh that was also silly

--- modules/security/smartcard.nix
+++ modules/security/smartcard.nix
@@ -31,56 +31,62 @@
   '';
 
   home-manager.sharedModules = [
-    ({ hmConfig, ... }: {
-      programs.gpg = {
-        enable = true;
-        homedir = "${hmConfig.xdg.dataHome}/gnupg";
-        scdaemonSettings = {
-          disable-ccid = true;
-          pcsc-shared = true;
+    (
+      hmInputs:
+      let
+        hmConfig = hmInputs.config;
+      in
+      {
+        programs.gpg = {
+          enable = true;
+          homedir = "${hmConfig.xdg.dataHome}/gnupg";
+          scdaemonSettings = {
+            disable-ccid = true;
+            pcsc-shared = true;
+          };
+          settings = {
+            keyserver = [
+              "hkps://keys.openpgp.org"
+              "hkps://keys.mailvelope.com"
+              "hkps://keyserver.ubuntu.com:443"
+              "hkps://pgpkeys.eu"
+              "hkp://zkaan2xfbuxia2wpf7ofnkbz6r5zdbbvxbunvp5g2iebopbfc4iqmbad.onion"
+            ];
+            auto-key-locate = "wkd,dane,local";
+            auto-key-retrieve = true;
+            list-options = [
+              "show-unusable-subkeys"
+              "show-uid-validity"
+            ];
+            verify-options = "show-uid-validity";
+            require-secmem = true;
+            with-key-origin = true;
+            with-fingerprint = true;
+            armor = true;
+            throw-keyids = true;
+            no-symkey-cache = true;
+          };
         };
-        settings = {
-          keyserver = [
-            "hkps://keys.openpgp.org"
-            "hkps://keys.mailvelope.com"
-            "hkps://keyserver.ubuntu.com:443"
-            "hkps://pgpkeys.eu"
-            "hkp://zkaan2xfbuxia2wpf7ofnkbz6r5zdbbvxbunvp5g2iebopbfc4iqmbad.onion"
-          ];
-          auto-key-locate = "wkd,dane,local";
-          auto-key-retrieve = true;
-          list-options = [
-            "show-unusable-subkeys"
-            "show-uid-validity"
-          ];
-          verify-options = "show-uid-validity";
-          require-secmem = true;
-          with-key-origin = true;
-          with-fingerprint = true;
-          armor = true;
-          throw-keyids = true;
-          no-symkey-cache = true;
+
+        services.gpg-agent = {
+          enable = true;
+          enableScDaemon = true;
+          enableExtraSocket = true;
+          enableSshSupport = true;
+
+          enableBashIntegration = true;
+          enableZshIntegration = true;
+          enableFishIntegration = true;
+          enableNushellIntegration = true;
+
+          verbose = true;
+
+          extraConfig = ''
+            ttyname $GPG_TTY
+            pinentry-program /run/current-system/sw/bin/pinentry
+          '';
         };
-      };
-
-      services.gpg-agent = {
-        enable = true;
-        enableScDaemon = true;
-        enableExtraSocket = true;
-        enableSshSupport = true;
-
-        enableBashIntegration = true;
-        enableZshIntegration = true;
-        enableFishIntegration = true;
-        enableNushellIntegration = true;
-
-        verbose = true;
-
-        extraConfig = ''
-          ttyname $GPG_TTY
-          pinentry-program /run/current-system/sw/bin/pinentry
-        '';
-      };
-    })
+      }
+    )
   ];
 }
