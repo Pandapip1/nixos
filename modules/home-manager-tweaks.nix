6034f1d9964b343fff3fd7cfb622ade5eaebc49e
Gavin John <gavinnjohn@gmail.com>
2026-07-29 12:57:46 -0400

    Fix hm profiles not setting correctly

--- modules/home-manager-tweaks.nix
+++ modules/home-manager-tweaks.nix
@@ -4,5 +4,14 @@
     useUserPackages = true;
     startAsUserService = true;
     verbose = true;
+    sharedModules = [
+      {
+        # Enable shell integration
+        programs.bash.enable = true;
+        programs.zsh.enable = true;
+        programs.fish.enable = true;
+        programs.nushell.enable = true;
+      }
+    ];
   };
 }
