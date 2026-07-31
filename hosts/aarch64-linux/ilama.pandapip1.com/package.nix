f7ae9e51974365a3b4e20f5618fb64c96bc39e1c
Gavin John <gavinnjohn@gmail.com>
2026-07-31 13:05:49 -0400

    Limit ilama nix configs

--- hosts/aarch64-linux/ilama.pandapip1.com/package.nix
+++ hosts/aarch64-linux/ilama.pandapip1.com/package.nix
@@ -6,5 +6,8 @@
   services.desktopManager.cosmic.enable = true;
   services.gnome.gnome-keyring.enable = false; # For some reason enabling cosmic enables gnome keyring. I want to use keepassxc thank you very much.
 
+  # Severely disk constrained
+  nix-gc.configurationLimit = 3;
+
   system.stateVersion = "26.11";
 }
