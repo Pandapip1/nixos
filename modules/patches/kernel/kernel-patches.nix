a562b24a9ab6085396b8c5a1b6d909dc8f943b95
Gavin John <gavinnjohn@gmail.com>
2026-08-01 16:02:10 -0400

    Reenable kernel patches

--- modules/patches/kernel/kernel-patches.nix
+++ modules/patches/kernel/kernel-patches.nix
@@ -1,12 +1,17 @@
 {
   boot.kernelPatches = [
-    # {
-    #   name = "resolution-quirk-match";
-    #   patch = ./0001-drm-edid-match-on-active-resolution.patch;
-    # }
-    # {
-    #   name = "xreal-light-non-desktop";
-    #   patch = ./0002-drm-edid-add-xreal-air-non-desktop-quirk-in-SBS.patch;
-    # }
+    {
+      name = "Rust Support";
+      patch = null;
+      features.rust = true;
+    }
+    {
+      name = "resolution-quirk-match";
+      patch = ./0001-drm-edid-match-on-active-resolution.patch;
+    }
+    {
+      name = "xreal-light-non-desktop";
+      patch = ./0002-drm-edid-add-xreal-air-non-desktop-quirk-in-SBS.patch;
+    }
   ];
 }
