c17b1cc80c5da6f80e9581bb98ee5445fa3787e4
Gavin John <gavinnjohn@gmail.com>
2026-08-03 14:40:35 -0400

    Singleton fonts

--- modules/rice/fonts.nix
+++ modules/rice/fonts.nix
@@ -1,6 +1,7 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }:
 
@@ -15,6 +16,8 @@ let
     ) (lib.filter (n: lib.hasPrefix "noto-fonts" n) (builtins.attrNames pkgs))
   );
   allNerdFonts = builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
+  singletonFontsDir = "${config.extraProfiles.singleton.path}/share/fonts";
 in
 {
   fonts = {
@@ -30,12 +33,33 @@ in
         emoji = [ "Noto Color Emoji" ];
       };
       useEmbeddedBitmaps = true;
+      localConf = ''
+        <?xml version='1.0'?>
+        <!DOCTYPE fontconfig SYSTEM 'urn:fontconfig:fonts.dtd'>
+        <fontconfig>
+          <dir>${singletonFontsDir}</dir>
+        </fontconfig>
+      '';
     };
-    packages =
-      allNerdFonts
-      ++ allNotoFonts
-      ++ (with pkgs; [
-        orbitron
-      ]);
+  };
+
+  extraProfiles.singleton.packages =
+    allNerdFonts
+    ++ allNotoFonts
+    ++ (with pkgs; [
+      orbitron
+    ]);
+
+  systemd.services.warm-singleton-font-cache = {
+    description = "Warm the fontconfig cache for the singleton profile's fonts";
+    after = [ "extra-profile-singleton.service" ];
+    requires = [ "extra-profile-singleton.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      if [ -d "${singletonFontsDir}" ]; then
+        ${lib.getExe' pkgs.fontconfig "fc-cache"} -f "${singletonFontsDir}"
+      fi
+    '';
   };
 }
