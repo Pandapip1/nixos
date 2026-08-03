9dec51a82790399063de259775dbecbf86788570
Gavin John <gavinnjohn@gmail.com>
2026-08-03 11:53:03 -0400

    Drop most packages by default

--- modules/air_quotes_bloat/chromium.nix
+++ modules/air_quotes_bloat/chromium.nix
@@ -5,9 +5,9 @@
   ...
 }:
 
-{
+lib.mkIf (config.services.graphical-desktop.enable && !(config.optimizations.lean.enable)) {
   programs.chromium = {
-    enable = lib.mkDefault config.services.graphical-desktop.enable;
+    enable = lib.mkDefault true;
   };
   environment.systemPackages = with pkgs; [
     ungoogled-chromium
