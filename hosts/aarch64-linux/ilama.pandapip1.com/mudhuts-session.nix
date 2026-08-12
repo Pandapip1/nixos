d8de9c34e5af30629ede4d82ff4a6446d152fac0
Gavin John <gavinnjohn@gmail.com>
2026-08-12 19:24:55 -0400

    Revert "Add muthuts logging"
    This reverts commit e1892824d030ec69e3c64a9e19282d985927ecf3.

--- hosts/aarch64-linux/ilama.pandapip1.com/mudhuts-session.nix
+++ hosts/aarch64-linux/ilama.pandapip1.com/mudhuts-session.nix
@@ -11,10 +11,7 @@
         destination = "/share/wayland-sessions";
         name = "mudhuts";
         comment = "mudhuts: a terminal-centric Wayland compositor";
-        exec = "${pkgs.writeShellScript "mudhuts-session" ''
-          export RUST_LOG=debug
-          exec ${lib.getExe pkgs.mudhuts} --tty > /tmp/mudhuts-greeter.log 2>&1
-        ''}";
+        exec = "${lib.getExe pkgs.mudhuts} --tty";
         type = "Application";
         desktopName = "mudhuts";
       }).overrideAttrs (oldAttrs: {
