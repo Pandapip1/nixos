41683f0eaeb9877391c8e5b57793186f8ce4f9ef
Gavin John <gavinnjohn@gmail.com>
2026-08-26 22:20:36 -0400

    Update flake

--- modules/air_quotes_bloat/grayjay.nix
+++ modules/air_quotes_bloat/grayjay.nix
@@ -9,7 +9,6 @@ lib.mkIf
   (
     config.services.graphical-desktop.enable
     && !(config.optimizations.lean.enable)
-    && (pkgs.stdenv.hostPlatform.system == "x86_64-linux")
   )
   {
     extraProfiles.singleton.packages = with pkgs; [
