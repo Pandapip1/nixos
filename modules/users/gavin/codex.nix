748fd3c851a78e47adf55e779a91e90f967df952
Gavin John <gavinnjohn@gmail.com>
2026-09-18 01:19:20 -0400

    Yea that doesn't work sad

--- modules/users/gavin/codex.nix
+++ modules/users/gavin/codex.nix
@@ -8,19 +8,19 @@
     pkgs.codex
   ];
 
-  home.file.".codex/config.toml".text = ''
-    model = "qwen3-5-9b-q4-k-m"
-    model_provider = "llamacpp"
-
-    [model_providers.llamacpp]
-    name = "llama.cpp"
-    base_url = "http://[::1]:8480/v1"
-    wire_api = "responses"
-    requires_openai_auth = false
-
-    [projects."/"]
-    trust_level = "trusted"
-  '';
+#  home.file.".codex/config.toml".text = ''
+#    model = "qwen3-5-9b-q4-k-m"
+#    model_provider = "llamacpp"#
+#
+#    [model_providers.llamacpp]
+#    name = "llama.cpp"
+#    base_url = "http://[::1]:8480/v1"
+#    wire_api = "responses"
+#    requires_openai_auth = false
+#
+#    [projects."/"]
+#    trust_level = "trusted"
+#  '';
 }
 )
   ];
