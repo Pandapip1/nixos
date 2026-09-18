b7ef0eaa5a5d7791bd38ff4a88592c3d4abc311a
Gavin John <gavinnjohn@gmail.com>
2026-09-18 15:57:42 -0400

    Working codex

--- hosts/aarch64-linux/ilama.pandapip1.com/llama.nix
+++ hosts/aarch64-linux/ilama.pandapip1.com/llama.nix
@@ -18,9 +18,65 @@ let
       file = "Qwen_Qwen3.5-9B-Q4_K_M.gguf";
       hash = "sha256-14TOntoaWntR6PcFqeYxCES/Txc2VNEVgjx3X96lbUM=";
 
-      settings.ctx-size = "262144";
+      settings.ctx-size = "57344";
+
+      patches = [
+        ./qwen3.5-developer.patch
+      ];
     };
   };
+
+  ggufPython = pkgs.python3.withPackages (ps: [
+    ps.gguf
+  ]);
+
+  mkChatTemplate =
+    {
+      model,
+      patches ? [ ],
+    }:
+    pkgs.runCommand "chat-template" {
+      nativeBuildInputs = [
+        ggufPython
+        pkgs.patch
+      ];
+
+      patchInputs = patches;
+    } ''
+      python - "${model}" "$TMPDIR/template.jinja" <<'PY'
+      import sys
+      from gguf import GGUFReader
+
+      model = sys.argv[1]
+      output = sys.argv[2]
+
+      reader = GGUFReader(model)
+      field = reader.get_field("tokenizer.chat_template")
+
+      if field is None:
+          raise SystemExit("GGUF does not contain tokenizer.chat_template")
+
+      template = field.contents()
+
+      if not isinstance(template, str):
+          raise TypeError(
+              f"tokenizer.chat_template has unexpected type: "
+              f"{type(template).__name__}"
+          )
+
+      with open(output, "w", encoding="utf-8") as f:
+          f.write(template)
+      PY
+
+      ${lib.concatMapStringsSep "\n" (
+        patch:
+        ''
+          patch "$TMPDIR/template.jinja" < ${patch}
+        ''
+      ) patches}
+
+      cp "$TMPDIR/template.jinja" "$out"
+    '';
 
   modelsIni = pkgs.writeText "llama-models.ini" (
     ''
@@ -33,11 +89,28 @@ let
     ''
     + builtins.concatStringsSep "\n" (
       lib.mapAttrsToList
-        (alias: model: ''
-          [${alias}]
-          model = ${pkgs.fetchurl { url = "https://huggingface.co/${model.repoId}/resolve/${model.rev}/${model.file}"; inherit (model) hash; }}
-          ctx-size = ${model.settings.ctx-size}
-        '')
+        (alias: model:
+          let
+            modelFile = pkgs.fetchurl {
+              url = "https://huggingface.co/${model.repoId}/resolve/${model.rev}/${model.file}";
+              inherit (model) hash;
+            };
+
+            template =
+              if model ? patches
+              then mkChatTemplate {
+                model = modelFile;
+                patches = model.patches;
+              }
+              else null;
+          in
+          ''
+            [${alias}]
+            model = ${modelFile}
+            ${lib.optionalString (template != null) "chat-template-file = ${template}"}
+            ctx-size = ${model.settings.ctx-size}
+          ''
+        )
         models
     )
   );
@@ -54,8 +127,9 @@ in
       models-preset = modelsIni;
 
       models-max = 1;
-
       models-autoload = true;
+
+    verbosity = 5;
     };
   };
 }
